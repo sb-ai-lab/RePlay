7932cdc6411069658dcefb247f0905d4cea42a52
Rose Aysina <rose.aysina@gmail.com>
2019-09-23 10:04:27 +0300

    rose, AILABRS-60-v1.0: пофиксила немного тесты

--- tests/models/test_popular_recommender.py
+++ tests/models/test_popular_recommender.py
@@ -14,42 +14,40 @@ class PopularRecommenderTestCase(PySparkTest):
     @parameterized.expand([
         # users, context, to_filter_seen_items, k, items
         # проверяем выделение айтемов
-        (["u1", "u2", "u3"], "no_context", False, 10, [["i1", 3 / 14],
-                                                       ["i2", 2 / 14],
-                                                       ["i3", 4 / 14],
-                                                       ["i4", 5 / 14],
-                                                       ["i999", 0.0]],),
-        (["u1", "u2", "u3"], "c1", False, 10, [["i1", 2 / 7],
-                                               ["i999", 0.0],
-                                               ["i998", 0.0],
-                                               ["i3", 3 / 7],
-                                               ["i4", 2 / 7]],),
-        (["u1", "u2", "u3"], "c2", False, 10, [["i1", 1 / 7],
-                                               ["i2", 2 / 7],
-                                               ["i3", 1 / 7],
-                                               ["i998", 0.0],
-                                               ["i999", 0.0],
-                                               ["i4", 3 / 7]],),
+        (["u1", "u2", "u3"], "no_context", 10, [["i1", 3 / 14],
+                                                ["i2", 2 / 14],
+                                                ["i3", 4 / 14],
+                                                ["i4", 5 / 14],
+                                                ["i999", 0.0]],),
+        (["u1", "u2", "u3"], "c1", 10, [["i1", 2 / 7],
+                                        ["i999", 0.0],
+                                        ["i998", 0.0],
+                                        ["i3", 3 / 7],
+                                        ["i4", 2 / 7]],),
+        (["u1", "u2", "u3"], "c2", 10, [["i1", 1 / 7],
+                                        ["i2", 2 / 7],
+                                        ["i3", 1 / 7],
+                                        ["i998", 0.0],
+                                        ["i999", 0.0],
+                                        ["i4", 3 / 7]],),
 
         # проверяем выделение юзеров
-        (["u1", "u2"], "no_context", False, 10, [["i1", 3 / 14],
-                                                 ["i2", 2 / 14],
-                                                 ["i3", 4 / 14],
-                                                 ["i4", 5 / 14],
-                                                 ["i999", 0.0]],),
+        (["u1", "u2"], "no_context", 10, [["i1", 3 / 14],
+                                          ["i2", 2 / 14],
+                                          ["i3", 4 / 14],
+                                          ["i4", 5 / 14],
+                                          ["i999", 0.0]],),
 
         # проверяем выделение топ-к
-        (["u1", "u2"], "c1", False, 1, [["i3", 3 / 7]]),
-        (["u1", "u3"], "c2", False, 2, [["i2", 2 / 7],
-                                        ["i4", 3 / 7]],),
-        (["u3", "u2"], "no_context", False, 3, [["i1", 3 / 14],
-                                                ["i3", 4 / 14],
-                                                ["i4", 5 / 14]]),
-
+        (["u1", "u2"], "c1", 1, [["i3", 3 / 7]]),
+        (["u1", "u3"], "c2", 2, [["i2", 2 / 7],
+                                 ["i4", 3 / 7]],),
+        (["u3", "u2"], "no_context", 3, [["i1", 3 / 14],
+                                         ["i3", 4 / 14],
+                                         ["i4", 5 / 14]]),
     ])
     def test_popularity_recs_no_params(self,
-                                       users, context,
-                                       to_filter_seen_items, k,
+                                       users, context, k,
                                        items_relevance
                                        ):
         log_data = [
@@ -84,26 +82,30 @@ class PopularRecommenderTestCase(PySparkTest):
             .withColumn('context', sf.lit(context))
 
         self.model.set_params(**{'alpha': 0, 'beta': 0})
-        _ = self.model.fit_predict(
-            k=k, users=users,
-            items=set([elem[0]
-                       for elem in items_relevance]),
-            context=context,
-            log=log,
-            user_features=None,
-            item_features=None,
-            to_filter_seen_items=to_filter_seen_items)
-        test_recs = self.model.fit_predict(
-            k=k, users=users,
-            items=set([elem[0]
-                       for elem in items_relevance]),
-            context=context,
-            log=log,
-            user_features=None,
-            item_features=None,
-            to_filter_seen_items=to_filter_seen_items)
 
-        self.assertSparkDataFrameEqual(true_recs, test_recs)
+        # два вызова нужны, чтобы проверить, что они возващают одно и то же
+        test_recs_first = self.model.fit_predict(
+            k=k, users=users,
+            items=set([elem[0]
+                       for elem in items_relevance]),
+            context=context,
+            log=log,
+            user_features=None,
+            item_features=None,
+            to_filter_seen_items=False)
+        test_recs_second = self.model.fit_predict(
+            k=k, users=users,
+            items=set([elem[0]
+                       for elem in items_relevance]),
+            context=context,
+            log=log,
+            user_features=None,
+            item_features=None,
+            to_filter_seen_items=False)
+
+        self.assertSparkDataFrameEqual(true_recs, test_recs_second)
+        self.assertSparkDataFrameEqual(true_recs, test_recs_first)
+        self.assertSparkDataFrameEqual(test_recs_first, test_recs_second)
 
 
 if __name__ == '__main__':
