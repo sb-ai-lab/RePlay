aa21fd6f8858290a148c7ffd6255c09f1acad04b
Rose Aysina <rose.aysina@gmail.com>
2019-09-26 16:34:28 +0300

    rose, AILABRS-29: добавила рандомное разделение длога в валидацию + тесты

--- sponge_bob_magic/validation_schemes.py
+++ sponge_bob_magic/validation_schemes.py
@@ -11,6 +11,7 @@ from pyspark.sql.types import TimestampType
 
 class ValidationSchemes:
     """ различные методы разбиения на обучающую и тестовую выборки """
+
     def __init__(self, spark: SparkSession):
         """
         :param spark: сессия Spark, в рамках которой будет происходить
@@ -37,7 +38,7 @@ class ValidationSchemes:
         :param drop_cold_users: исключать ли из тестовой выборки пользователей,
         которых нет в обучающей
         :returns: тройка таблиц структуры, аналогичной входной
-        (train, test_inpit, test)
+        (train, test_input, test)
         """
         train = log.filter(
             col("timestamp") < lit(test_start).cast(TimestampType())
@@ -45,6 +46,44 @@ class ValidationSchemes:
         test = log.filter(
             col("timestamp") >= lit(test_start).cast(TimestampType())
         )
+        if drop_cold_items:
+            test = test.join(
+                train.select("item_id").distinct(),
+                how="inner",
+                on="item_id"
+            )
+        if drop_cold_users:
+            test = test.join(
+                train.select("user_id").distinct(),
+                how="inner",
+                on="user_id"
+            )
+        return train, train, test
+
+    @staticmethod
+    def log_split_randomly(
+            log: DataFrame,
+            test_size: float,
+            drop_cold_items: bool,
+            drop_cold_users: bool,
+            seed: int = 1234
+    ) -> Tuple[DataFrame, DataFrame, DataFrame]:
+        """
+        разбить лог действий пользователей рандомно на обучающую и тестовую
+        выборки
+
+        :param seed: рандомный сид, нужен для повторения разбиения
+        :param test_size: размер тестовой выборки, от 0 до 1
+        :param log: таблица с колонками
+        (timestamp, user_id, item_id, context, relevance)
+        :param drop_cold_items: исключать ли из тестовой выборки объекты,
+        которых нет в обучающей
+        :param drop_cold_users: исключать ли из тестовой выборки пользователей,
+        которых нет в обучающей
+        :returns: тройка таблиц структуры, аналогичной входной
+        (train, test_input, test)
+        """
+        train, test = log.randomSplit([1 - test_size, test_size], seed)
         if drop_cold_items:
             test = test.join(
                 train.select("item_id").distinct(),
