3ea713800716d2642a9f7b56c2e7c9f6394827e3
Rose Aysina <rose.aysina@gmail.com>
2019-09-27 15:26:38 +0300

    rose, AILABRS-60-v1.0: доки для utils

--- sponge_bob_magic/utils.py
+++ sponge_bob_magic/utils.py
@@ -1,7 +1,16 @@
+from typing import Set, Any
+
 from pyspark.sql import DataFrame
 
 
-def get_distinct_values_in_column(df: DataFrame, column: str):
+def get_distinct_values_in_column(df: DataFrame, column: str) -> Set[Any]:
+    """
+    возвращает уникальные значения в колонке spark-датафрейма в виде python set
+
+    :param df: spark-датафрейм
+    :param column: имя колонки
+    :return: уникальные значения в колонке
+    """
     return set([row[column]
                 for row in (df
                             .select(column)
