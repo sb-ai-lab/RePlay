16d9e5909f0257fbc80b0dc66887ed0103c71d26
alside <asidorenkoa@yandex.ru>
2019-10-07 14:31:12 +0300

    add ndcg@k and tested

--- sponge_bob_magic/metrics/metrics.py
+++ sponge_bob_magic/metrics/metrics.py
@@ -3,6 +3,10 @@
 """
 from pyspark.sql import DataFrame, Window
 from pyspark.sql.functions import col, desc, row_number
+from pyspark.mllib.evaluation import RankingMetrics
+from pyspark.ml.feature import StringIndexer
+from pyspark.sql import functions as F
+
 
 
 class Metrics:
@@ -40,3 +44,38 @@ class Metrics:
             top_k_recommendations.select("user_id").distinct().count()
         )
         return users_hit / users_total
+
+    @staticmethod
+    def ndcg_at_k(
+            recommendations: DataFrame,
+            ground_truth: DataFrame,
+            k: int
+    ) -> float:
+        """
+        ndcg: чем релевантнее элементы среди первых `k`, тем лучше
+
+        :param recommendations: выдача рекомендательной системы вида
+        (user_id, item_id, context, relevance)
+        :param ground_truth: реальный лог действий пользователей
+        :param k: какое максимальное количество объектов брать из топа
+        рекомендованных для оценки
+        """
+
+        indexer = StringIndexer(inputCol="item_id", outputCol="item_idx", handleInvalid='keep').fit(ground_truth)
+        df_true = indexer.transform(ground_truth)
+        df_pred = indexer.transform(recommendations)
+
+        df_pred = df_pred.groupby("user_id").agg(F.collect_list("item_idx").alias('pred_items'))
+        df_true = df_true.groupby("user_id").agg(F.collect_list("item_idx").alias('true_items'))
+
+        predictionAndLabels = df_pred \
+            .join(df_true, ['user_id'], how='full') \
+            .rdd \
+            .map(lambda row: (row[1] if row[1] is not None else [],
+                              row[2] if row[2] is not None else [])
+                 )
+        metrics = RankingMetrics(predictionAndLabels)
+
+        return metrics.ndcgAt(k)
+
+
