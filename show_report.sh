8cfa1a8695c8400ab48e7a252a3e3076dde8bee9
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2019-10-07 15:48:31 +0300

    не считать количество строчек в тестах, но проверять их pylint

--- show_report.sh
+++ show_report.sh
@@ -3,7 +3,7 @@
 PACKAGE_NAME=sponge_bob_magic
 pycodestyle ${PACKAGE_NAME} tests
 mypy ${PACKAGE_NAME} tests
-pylint ${PACKAGE_NAME}
+pylint ${PACKAGE_NAME} tests
 coverage run --source ${PACKAGE_NAME} -m unittest discover -s tests/
 coverage report -m
-cloc ${PACKAGE_NAME} tests
+cloc ${PACKAGE_NAME}
