3d195c82d1c1babe85f2015a2a03831288addc7c
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2019-10-11 15:22:14 +0300

    пофиксить проблемы с mypy

--- show_report.sh
+++ show_report.sh
@@ -2,7 +2,7 @@
 
 PACKAGE_NAME=sponge_bob_magic
 pycodestyle ${PACKAGE_NAME} tests
-mypy ${PACKAGE_NAME} tests
+mypy --ignore-missing-imports ${PACKAGE_NAME} tests
 pylint ${PACKAGE_NAME} tests
 coverage run --source ${PACKAGE_NAME} -m unittest discover -s tests/
 coverage report -m
