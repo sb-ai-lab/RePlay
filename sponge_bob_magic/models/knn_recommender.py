f1097f8ecc98b0af2d5c186c766935ad5d604ff1
Rose Aysina <rose.aysina@gmail.com>
2019-10-23 10:48:46 +0300

    rose, AILABRS-70: переименовала k в KNN и поправила док

--- sponge_bob_magic/models/knn_recommender.py
+++ sponge_bob_magic/models/knn_recommender.py
@@ -20,15 +20,17 @@ class KNNRecommender(BaseRecommender):
     item_norms: Optional[DataFrame]
     similarity: Optional[DataFrame]
 
-    def __init__(self, spark: SparkSession, k: int, shrink: float = 0.0):
+    def __init__(self, spark: SparkSession,
+                 num_neighbours: int,
+                 shrink: float = 0.0):
         super().__init__(spark)
 
         self.shrink = shrink
-        self.k = k
+        self.num_neighbours = num_neighbours
 
     def get_params(self) -> Dict[str, object]:
         return {"shrink": self.shrink,
-                "k": self.k}
+                "num_neighbours": self.num_neighbours}
 
     def _get_similarity_matrix(
             self,
@@ -37,8 +39,8 @@ class KNNRecommender(BaseRecommender):
             item_norms: DataFrame
     ) -> DataFrame:
         """
-        получить верхнюю треугольную матрицу модифицированной косинусной меры
-        схожести
+        Получает верхнюю треугольную матрицу модифицированной косинусной меры
+        схожести.
 
         :param items: объекты, между которыми нужно посчитать схожесть,
             спарк-датафрейм с колонкой `[item_id]`
@@ -107,7 +109,7 @@ class KNNRecommender(BaseRecommender):
                     Window.partitionBy("item_id_one").orderBy("similarity")
                 )
             )
-            .filter(sf.col("similarity_order") <= self.k)
+            .filter(sf.col("similarity_order") <= self.num_neighbours)
             .drop("similarity_order")
             .cache()
         )
