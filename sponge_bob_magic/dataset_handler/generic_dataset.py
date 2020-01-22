7a70721ed1f84040e185a16efc1ad0a084538a51
18235210 <yytamm@sberbank.ru>
2020-01-22 16:04:23 +0300

    хотфикс

--- sponge_bob_magic/dataset_handler/generic_dataset.py
+++ sponge_bob_magic/dataset_handler/generic_dataset.py
@@ -3,6 +3,7 @@ from os.path import join
 
 import pandas as pd
 from IPython.core.display import display
+from pandas import DataFrame
 
 
 class Dataset:
@@ -18,6 +19,7 @@ class Dataset:
     def info(self):
         with pd.option_context('display.max_columns', 10):
             for name, df in self.__dict__.items():
-                print(name)
-                display(df.head(3))
-                print()
+                if isinstance(df, DataFrame):
+                    print(name)
+                    display(df.head(3))
+                    print()
