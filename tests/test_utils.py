28cc1e421f4524018d091bc211de9e8ed0ae8362
18330005 <avavasilyev@sigma.sbrf.ru>
2020-02-11 10:58:46 +0300

    Исправил багу в тесте, связанную с преждевременной остановкой спарка

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -64,4 +64,3 @@ class UtilsTestCase(PySparkTest):
         spark = utils.get_spark_session(1)
         self.assertIsInstance(spark, SparkSession)
         self.assertEqual(spark.conf.get("spark.driver.memory"), "1g")
-        spark.stop()
