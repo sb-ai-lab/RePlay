8e780c0452f0a32d5823a62e459fd6aeaad19e64
18330005 <avavasilyev@sigma.sbrf.ru>
2020-02-14 16:52:11 +0300

    правка метрик(не до конца работает)

--- sponge_bob_magic/metrics.py
+++ sponge_bob_magic/metrics.py
@@ -4,12 +4,12 @@
 import logging
 from abc import ABC, abstractmethod
 from math import log2
-from typing import Union
+from typing import Union, Iterable, Dict, Callable
 
 from pyspark.ml.feature import StringIndexer
 from pyspark.mllib.evaluation import RankingMetrics
 from pyspark.rdd import RDD
-from pyspark.sql import DataFrame, Window
+from pyspark.sql import DataFrame, Window, Column
 from pyspark.sql import functions as sf
 
 from sponge_bob_magic.utils import get_top_k_recs
@@ -24,7 +24,7 @@ class Metric(ABC):
             self,
             recommendations: DataFrame,
             ground_truth: DataFrame,
-            k: int
+            k: Iterable[int]
     ) -> NumType:
         """
         :param recommendations: выдача рекомендательной системы,
@@ -33,7 +33,7 @@ class Metric(ABC):
         :param ground_truth: реальный лог действий пользователей,
             спарк-датафрейм вида
             `[user_id , item_id , timestamp , context , relevance]`
-        :param k: какое максимальное количество объектов брать из топа
+        :param k: список индексов, показывающий какое максимальное количество объектов брать из топа
             рекомендованных для оценки
         :return: значение метрики
         """
@@ -49,7 +49,7 @@ class Metric(ABC):
             self,
             recommendations: DataFrame,
             ground_truth: DataFrame,
-            k: int
+            k: Iterable[int]
     ) -> NumType:
         """
         Расчёт значения метрики
@@ -91,7 +91,7 @@ class Metric(ABC):
         (см. `pyspark.mllib.evaluation.RankingMetrics`).
 
         :param recommendations: выдача рекомендательной системы,
-            спарк-датарейм вида
+            спарк-датафрейм вида
             `[user_id, item_id, context, relevance]`
         :param ground_truth: реальный лог действий пользователей,
             спарк-датафрейм вида
@@ -105,7 +105,7 @@ class Metric(ABC):
                 outputCol="item_idx",
                 handleInvalid="keep"
             )
-            .fit(ground_truth)
+                .fit(ground_truth)
         )
         df_true = indexer.transform(ground_truth)
         df_pred = indexer.transform(recommendations)
@@ -124,10 +124,64 @@ class Metric(ABC):
                    .agg(sf.max('true_items').alias('true_items')))
         prediction_and_labels = (
             df_pred
-            .join(df_true, ["user_id"], how="inner")
-            .rdd
+                .join(df_true, ["user_id"], how="inner")
+                .rdd
         )
         return prediction_and_labels
+
+    @staticmethod
+    def _get_metrics_diff_top_k(metric_elements: DataFrame,
+                                k: Iterable[int],
+                                agg_fn: Callable[[Column], Column]
+                                ) -> Dict[int, NumType]:
+        """
+            Рассчитывает усредненную метрику по различным топ-k рекомендаций из набора.
+
+            :param metric_elements: элеменеты ряда для расчета метрики, спарк-датафрейм с колонками
+                `[user_id , item_id , context , metric]`
+            :param k: набор количеств рекомендаций, по которым необходимо подсчитать метрики
+            :return: топ-k рекомендации, словарь с key - количество рекомендаций и value - значение метрики
+        """
+        k_set = set(k)
+        window = (Window
+                  .partitionBy(metric_elements["user_id"])
+                  .orderBy(metric_elements["relevance"].desc()))
+
+        cum_agg = (metric_elements
+                   .withColumn('k',
+                               sf.count(metric_elements.relevance).over(window))
+                   .withColumn('cum_agg',
+                               agg_fn(metric_elements.metric).over(window))
+                   .where(sf.col("k").isin(k_set)))
+        total_metric = (cum_agg
+                        .groupby("k").agg(
+            sf.avg("cum_agg").alias("total_metric"))
+                        .select("total_metric", "k")).collect()
+
+        return {row["k"]: row["total_metric"] for row in total_metric}
+
+    @staticmethod
+    def _merge_prediction_and_truth_top_k(
+            recommendations: DataFrame,
+            ground_truth: DataFrame,
+            k: int
+    ) -> DataFrame:
+        top_k_recommendations = get_top_k_recs(recommendations, k)
+        true_items_by_users = (ground_truth
+            .groupby("user_id").agg(
+                sf.collect_set("item_id").alias("items_id")))
+
+        @sf.udf("int")
+        def check_is_in(item_id,items_id):
+            return int(item_id in items_id)
+
+        merged_dataframe = top_k_recommendations.join(
+            true_items_by_users,
+            how="inner",
+            on=["user_id"]
+        ).withColumn("metric", check_is_in(sf.col("item_id"),sf.col("items_id")))
+
+        return merged_dataframe
 
 
 class HitRate(Metric):
@@ -145,18 +199,11 @@ class HitRate(Metric):
             self,
             recommendations: DataFrame,
             ground_truth: DataFrame,
-            k: int
-    ) -> NumType:
-        top_k_recommendations = get_top_k_recs(recommendations, k)
-        users_hit = top_k_recommendations.join(
-            ground_truth,
-            how="inner",
-            on=["user_id", "item_id", "context"]
-        ).select("user_id").distinct().count()
-        users_total = (
-            top_k_recommendations.select("user_id").distinct().count()
-        )
-        return users_hit / users_total
+            k: Iterable[int]
+    ) -> Dict[int, NumType]:
+        dataframe = self._merge_prediction_and_truth_top_k(recommendations, ground_truth, max(k))
+
+        return self._get_metrics_diff_top_k(dataframe, k, sf.max)
 
 
 class NDCG(Metric):
@@ -173,14 +220,31 @@ class NDCG(Metric):
             self,
             recommendations: DataFrame,
             ground_truth: DataFrame,
-            k: int
-    ) -> NumType:
-        dataframe = self._merge_prediction_and_truth(
-            recommendations, ground_truth
-        )
-        dataframe = dataframe.map(lambda row: (row[1], row[2]))
-        metrics = RankingMetrics(dataframe)
-        return metrics.ndcgAt(k)
+            k: Iterable[int]
+    ) -> Dict[int, NumType]:
+        dataframe = NDCG._merge_prediction_and_truth_top_k(recommendations, ground_truth, max(k))
+
+        #@sf.pandas_udf("double", sf.PandasUDFType.GROUPED_MAP)
+        @sf.udf("double")
+        def mean_udf(v):
+            return v.mean()
+
+        return self._get_metrics_diff_top_k(dataframe, k, mean_udf)
+
+        # dataframe = self._merge_prediction_and_truth(
+        #     recommendations, ground_truth
+        # )
+        #
+        # def _calc_metric_elem(row):
+        #     true_items_set = set(row[2])
+        #     return [{"user_id": row["user_id"], "item_id": item_id, "metric": int(item_id in true_items_set),
+        #              "relevance": ind} for ind, item_id in enumerate(row["pred_items"])]
+        #
+        # dataframe = dataframe.flatMap(_calc_metric_elem).toDF()
+        # # dataframe = dataframe.map(lambda row: (row[1], row[2]))
+        # # print(dataframe.take(10))
+        # # metrics = RankingMetrics(dataframe)
+        # return dataframe  # metrics.ndcgAt(k)
 
 
 class Precision(Metric):
@@ -256,21 +320,21 @@ class Recall(Metric):
         ).groupby("user_id").agg(sf.count("item_id").alias("hits"))
         totals = (
             ground_truth
-            .groupby("user_id")
-            .agg(sf.count("item_id").alias("totals"))
+                .groupby("user_id")
+                .agg(sf.count("item_id").alias("totals"))
         )
         total_recall, total_users = (
             totals.join(hits, on=["user_id"], how="left")
-            .withColumn(
+                .withColumn(
                 "recall",
                 sf.coalesce(sf.col("hits") / sf.col("totals"), sf.lit(0))
             )
-            .agg(
+                .agg(
                 sf.sum("recall").alias("total_hits"),
                 sf.count("recall").alias("total_users")
             )
-            .select("total_hits", "total_users")
-            .head()[:2]
+                .select("total_hits", "total_users")
+                .head()[:2]
         )
         return total_recall / total_users
 
@@ -337,14 +401,14 @@ class Surprisal(Metric):
                                           how="left").fillna(self.fill_value)
         list_mean = (
             recs
-            .groupby("user_id")
-            .agg(sf.mean(metric).alias(metric))
+                .groupby("user_id")
+                .agg(sf.mean(metric).alias(metric))
         )
 
         global_mean = (
             list_mean
-            .select(sf.mean(metric).alias("mean"))
-            .collect()[0]["mean"]
+                .select(sf.mean(metric).alias("mean"))
+                .collect()[0]["mean"]
         )
 
         return global_mean
