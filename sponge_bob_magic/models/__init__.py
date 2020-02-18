3affd02da649a040f34182d119e81a134d98ed2f
18235210 <Neurosber1>
2020-02-18 14:06:29 +0300

    Дополнительный текст в первой половине ноутбука и иниты

--- sponge_bob_magic/models/__init__.py
+++ sponge_bob_magic/models/__init__.py
@@ -1,1 +1,8 @@
 """Документация для модуля models"""
+
+from sponge_bob_magic.models.als_rec import ALSRec
+from sponge_bob_magic.models.knn_rec import KNNRec
+from sponge_bob_magic.models.lightfm_rec import LightFMRec
+from sponge_bob_magic.models.linear_rec import LinearRec
+from sponge_bob_magic.models.neuromf_rec import NeuroMFRec
+from sponge_bob_magic.models.pop_rec import PopRec
