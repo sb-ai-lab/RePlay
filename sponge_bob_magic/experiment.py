9e5b7cf56b42ef616d77e70b55da8074c02a9a84
18235210 <Neurosber1>
2020-02-19 14:00:04 +0300

    Эксперимент принимает словарь что как считать

--- sponge_bob_magic/experiment.py
+++ sponge_bob_magic/experiment.py
@@ -1,5 +1,5 @@
 from collections import Iterable
-from typing import Any, List
+from typing import Any, List, Union, Dict
 
 import pandas as pd
 
@@ -13,37 +13,50 @@ class Experiment:
 
     Пример:
     >>> import pandas as pd
-    >>> from sponge_bob_magic.metrics import NDCG
+    >>> from sponge_bob_magic.metrics import NDCG, Surprisal
     >>> from sponge_bob_magic.experiment import Experiment
+    >>> from sponge_bob_magic.converter import convert
+    >>> log = pd.DataFrame({"user_id": [2,2,2,1], "item_id": [1,2,3,3], "relevance": [5,5,5,5]})
+    >>> log = convert(log)
     >>> test = pd.DataFrame({"user_id": [1,1,1], "item_id": [1,2,3], "relevance": [5,3,4]})
-    >>> pred = pd.DataFrame({"user_id": [1,1,1], "item_id": [1,2,4], "relevance": [5,4,5]})
-    >>> ex = Experiment(test, NDCG())
-    >>> ex.add_result('my_model', pred, 3)
+    >>> pred = pd.DataFrame({"user_id": [1,1,1], "item_id": [1,3,4], "relevance": [5,4,5]})
+    >>> ex = Experiment(test, {NDCG(): [2,3], Surprisal(log): 3})
+    >>> ex.add_result('my_model', pred)
     >>> ex.df
-                nDCG@3
-    my_model  0.703918
+              Surprisal@3    nDCG@2    nDCG@3
+    my_model     0.666667  0.613147  0.703918
 
     """
-    def __init__(self, test: Any, metrics: List[Metric]):
+    def __init__(self, test: Any, metrics: Dict[Metric, Union[int, List[int]]]):
         """
         :param test: Данные для теста в формате pandas или pyspark DataFrame
-        :param metrics: Список метрик, которые необходимо считать
+        :param metrics: Словарь метрик, которые необходимо считать.
+            Ключ -- метрика, значение -- int или список интов, обозначающих k,
+            для которых необходимо посчитать метрику.
         """
         self.test = convert(test)
-        if not isinstance(metrics, Iterable):
-            metrics = [metrics]
-        self.metrics = metrics
+        self.metrics = self._verify(metrics)
         self.df = pd.DataFrame()
 
-    def add_result(self, name: str, pred: Any, k: int):
+    @staticmethod
+    def _verify(metrics):
+        """Проверяет корректность аргумента"""
+        if not isinstance(metrics, dict):
+            raise TypeError(f"metrics argument must be a dictionary, got {type(metrics)}")
+        for metric, k in metrics.items():
+            if not isinstance(k, Iterable):
+                metrics[metric] = [k]
+        return metrics
+
+    def add_result(self, name: str, pred: Any):
         """
         Подсчитать метрики для переданного списка рекомендаций
         :param name: имя модели/эксперимента для сохранения результатов
         :param pred: список рекомендаций для подсчета метрик
-        :param k: конкретное значение k для metric@k
         """
         res = pd.Series(name=name)
         recs = convert(pred)
-        for metric in self.metrics:
-            res[f"{metric}@{k}"] = metric(recs, self.test, k)
+        for metric, ks in self.metrics.items():
+            for k in ks:
+                res[f"{metric}@{k}"] = metric(recs, self.test, k)
         self.df = self.df.append(res)
