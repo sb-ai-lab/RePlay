66ba16a852bb8d95b659bce1e782abf46f955d8e
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2020-03-16 15:05:19 +0300

    добавить сборку доки в Jenkins

--- test_package.sh
+++ test_package.sh
@@ -5,3 +5,6 @@ source ./venv/bin/activate
 pycodestyle --ignore=E501,W605,W504 --max-doc-length=160 ${PACKAGE_NAME} tests
 coverage run --source ${PACKAGE_NAME} -m pytest -s tests/
 coverage report -m
+cd docs
+mkdir -p _static
+make clean html
