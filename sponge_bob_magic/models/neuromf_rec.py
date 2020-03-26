effb397f566f70333eba4c19362c11244793ff9b
18330005 <avavasilyev@sigma.sbrf.ru>
2020-03-26 12:41:44 +0300

    вынес принты в логгер

--- sponge_bob_magic/models/neuromf_rec.py
+++ sponge_bob_magic/models/neuromf_rec.py
@@ -260,22 +260,22 @@ class NeuroMFRec(Recommender):
 
         @self.trainer.on(Events.EPOCH_COMPLETED)
         def log_training_loss(trainer):
-            print("Epoch[{}] Loss: {:.4f}".format(
-                trainer.state.epoch, trainer.state.output))
+            logging.debug("Epoch[{}] current loss: {:.4f}"
+                          .format(trainer.state.epoch, trainer.state.output))
 
         @self.trainer.on(Events.EPOCH_COMPLETED)
         def log_training_results(trainer):
             self.train_evaluator.run(train_data_loader)
             metrics = self.train_evaluator.state.metrics
-            print("Training set Results - Epoch: {} Avg loss: {:.4f}"
-                  .format(trainer.state.epoch, metrics['loss']))
+            logging.debug("Epoch[{}] training average loss: {:.4f}"
+                          .format(trainer.state.epoch, metrics['loss']))
 
         @self.trainer.on(Events.EPOCH_COMPLETED)
         def log_validation_results(trainer):
             self.val_evaluator.run(val_data_loader)
             metrics = self.val_evaluator.state.metrics
-            print("Validation set Results - Epoch: {} Avg loss: {:.4f}"
-                  .format(trainer.state.epoch, metrics['loss']))
+            logging.debug("Epoch[{}] validation average loss: {:.4f}"
+                          .format(trainer.state.epoch, metrics['loss']))
 
         def score_function(engine):
             return -engine.state.metrics['loss']
