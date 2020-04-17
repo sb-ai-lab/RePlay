d2ffd809ab192874d1cf55a2c2257e9a17b61402
18235210 <yytamm@sberbank.ru>
2020-04-17 19:30:10 +0300

    прощай, датасет

--- sponge_bob_magic/converter.py
+++ sponge_bob_magic/converter.py
@@ -4,13 +4,11 @@
 from pandas import DataFrame as PandasDataFrame
 from pyspark.sql import DataFrame as SparkDataFrame
 
-from rs_datasets.generic_dataset import Dataset
 from sponge_bob_magic.session_handler import State
 
 SPARK = "spark"
 PANDAS = "pandas"
-DATASET = "dataset"
-supported_types = [SPARK, PANDAS, DATASET]
+supported_types = [SPARK, PANDAS]
 
 
 def convert(df, type_out=SPARK):
@@ -27,11 +25,6 @@ def convert(df, type_out=SPARK):
     type_in = get_type(df)
     if type_in == type_out:
         return df
-    elif type_in == DATASET:
-        for name, data in df.__dict__.items():
-            type_in = get_type(data, except_unknown=False)
-            if type_in in supported_types:
-                df.__dict__[name] = convert(df.__dict__[name], type_out)
     elif type_out == SPARK:
         spark = State().session
         return spark.createDataFrame(df)
@@ -45,8 +38,6 @@ def get_type(obj, except_unknown=True):
         res = PANDAS
     elif isinstance(obj, SparkDataFrame):
         res = SPARK
-    elif isinstance(obj, Dataset):
-        res = DATASET
     elif except_unknown:
         raise NotImplementedError(f"{type(obj)} conversion is not implemented")
     else:
