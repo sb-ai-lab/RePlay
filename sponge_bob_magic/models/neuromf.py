c156064e36d496797446e1a599590e7f2c9f40c3
18330005 <avavasilyev@sigma.sbrf.ru>
2020-04-20 11:38:12 +0300

    правки

--- sponge_bob_magic/models/neuromf.py
+++ sponge_bob_magic/models/neuromf.py
@@ -264,11 +264,11 @@ class NeuroMF(Recommender):
             raise ValueError("Хотя бы один из параметров embedding_gmf_dim, "
                              "embedding_mlp_dim должен быть не пуст")
 
-        if ((embedding_gmf_dim is None  or embedding_gmf_dim < 0) and
+        if ((embedding_gmf_dim is None or embedding_gmf_dim < 0) and
            (embedding_mlp_dim is None or embedding_mlp_dim < 0)):
             raise ValueError("Параметры embedding_gmf_dim, embedding_mlp_dim"
                              " должны быть положительными")
-        
+
         self.device = State().device
         self.learning_rate = learning_rate
         self.epochs = epochs
