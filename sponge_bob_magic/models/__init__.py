be46b2542a496f54d8c96d7303af64b026a32e9a
18235210 <yytamm@sberbank.ru>
2020-04-28 15:51:38 +0300

    merge master

--- sponge_bob_magic/models/__init__.py
+++ sponge_bob_magic/models/__init__.py
@@ -7,6 +7,7 @@
 
 from sponge_bob_magic.models.als import ALSWrap
 from sponge_bob_magic.models.base_rec import Recommender
+from sponge_bob_magic.models.base_torch_rec import TorchRecommender
 from sponge_bob_magic.models.classifier_rec import ClassifierRec
 from sponge_bob_magic.models.knn import KNN
 from sponge_bob_magic.models.lightfm import LightFMWrap
@@ -15,3 +16,4 @@ from sponge_bob_magic.models.neuromf import NeuroMF
 from sponge_bob_magic.models.pop_rec import PopRec
 from sponge_bob_magic.models.random_rec import RandomRec
 from sponge_bob_magic.models.slim import SLIM
+from sponge_bob_magic.models.wilson import Wilson
