48b1867135a00e89cfa04397f55d1fd833d12bd3
18235210 <yytamm@sberbank.ru>
2020-04-30 19:09:45 +0300

    получай, кавередж!

--- sponge_bob_magic/models/implicit.py
+++ sponge_bob_magic/models/implicit.py
@@ -8,7 +8,7 @@ from sponge_bob_magic.models import Recommender
 from sponge_bob_magic.utils import to_csr
 
 
-class ImplicitWrap(Recommender):
+class ImplicitWrap(Recommender):  # pragma: no cover
     """Обертка для пакета `implicit
     <https://github.com/benfred/implicit>`_"""
 
