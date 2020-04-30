2ff17b818bd8e44d64ee4c227073f2ecceb8b993
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2020-04-30 11:34:29 +0300

    включить pylint в процесс сборки

--- show_report.sh
+++ show_report.sh
@@ -2,7 +2,5 @@
 
 PACKAGE_NAME=sponge_bob_magic
 mypy --ignore-missing-imports ${PACKAGE_NAME} tests
-pylint --rcfile=.pylintrc ${PACKAGE_NAME}
-export SPARK_LOCAL_IP=127.0.0.1
 cloc ${PACKAGE_NAME}
 ./test_package.sh
