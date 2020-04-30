2dad3442763db438d3b9b482159d4ac2284fd8a4
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2020-04-30 16:40:59 +0300

    вынести exit-zero в вызов

--- test_package.sh
+++ test_package.sh
@@ -8,7 +8,7 @@ mkdir -p _static
 make clean html
 cd ..
 pycodestyle --ignore=E501,W605,W503,E203,E231 --max-doc-length=160 ${PACKAGE_NAME} tests
-pylint --rcfile=.pylintrc ${PACKAGE_NAME}
+pylint --rcfile=.pylintrc --exit-zero ${PACKAGE_NAME}
 export PYTEST_RUNNING=Y
 pytest --cov=${PACKAGE_NAME} --cov-report=term-missing \
        --doctest-modules ${PACKAGE_NAME} --cov-fail-under=93 tests
