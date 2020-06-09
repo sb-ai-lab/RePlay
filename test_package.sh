a002858e271007ea3bc14384ce0fffc9059a8fb5
Шминке Борис Андреевич <Shminke.B.A@sberbank.ru>
2020-06-09 12:25:52 +0300

    заваливать сборку, если pylint ругается

--- test_package.sh
+++ test_package.sh
@@ -9,7 +9,7 @@ mkdir -p _static
 make clean html
 cd ..
 pycodestyle --ignore=E203,E501,W503,W605 --max-doc-length=160 ${PACKAGE_NAME} tests
-pylint --rcfile=.pylintrc --exit-zero ${PACKAGE_NAME}
+pylint --rcfile=.pylintrc ${PACKAGE_NAME}
 mypy --ignore-missing-imports ${PACKAGE_NAME} tests
 export PYTEST_RUNNING=Y
 pytest --cov=${PACKAGE_NAME} --cov-report=term-missing \
