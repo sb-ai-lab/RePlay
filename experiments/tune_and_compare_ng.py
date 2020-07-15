9e9ae1f15ae67263493f84924bd9f902b07ce155
18235210 <yytamm@sberbank.ru>
2020-07-15 16:51:02 +0300

    подбор параметров и посчет метрик

---
+++ experiments/tune_and_compare_ng.py
@@ -0,0 +1,95 @@
+# type: ignore
+# pylint: disable-all
+from sponge_bob_magic.metrics import *
+from sponge_bob_magic.models import *
+from rs_datasets import MovieLens
+from datetime import datetime
+
+from sponge_bob_magic.models import PopRec
+from sponge_bob_magic.splitters import UserSplitter
+from sponge_bob_magic.experiment import Experiment
+
+from implicit.als import AlternatingLeastSquares
+from implicit.bpr import BayesianPersonalizedRanking
+
+import nevergrad as ng
+
+seed = 1337
+budget = 20
+params = dict()
+k = [5, 10, 50, 100]
+start = datetime.now()
+for dataset in ["1m", "10m"]:
+    print(f"starting {dataset}")
+    ml = MovieLens(dataset)
+    df = ml.ratings
+    df["relevance"] = df.rating
+    params[dataset] = dict()
+
+    splitter = UserSplitter(0.2, shuffle=True, drop_cold_items=True, seed=seed)
+    train, test = splitter.split(df)
+    train, val = splitter.split(train)
+
+    def train_als(factors, regularization):
+        model = AlternatingLeastSquares(factors, regularization)
+        return train_implicit(model)
+
+    def train_bpr(factors, regularization, learning_rate):
+        model = BayesianPersonalizedRanking(
+            factors, learning_rate, regularization
+        )
+        return train_implicit(model)
+
+    def train_implicit(model):
+        model = ImplicitWrap(model)
+        pred = model.fit_predict(train, k=max(k))
+        return -NDCG()(pred, val, max(k))
+
+    e = Experiment(test, {NDCG(): k, HitRate(): k, MRR(): k, Recall(): k,},)
+
+    lr = ng.p.Log(lower=0.0001, upper=1.0)
+    reg = ng.p.Log(lower=0.001, upper=1.0)
+    factors = ng.p.Scalar(lower=5, upper=300).set_integer_casting()
+
+    parametrization = ng.p.Instrumentation(
+        regularization=reg, factors=factors,
+    )
+
+    optimizer = ng.optimizers.OnePlusOne(
+        parametrization=parametrization, budget=budget
+    )
+    recommendation = optimizer.minimize(train_als)
+    model = AlternatingLeastSquares(**recommendation.kwargs)
+    model = ImplicitWrap(model)
+    pred = model.fit_predict(train, k=max(k))
+    e.add_result("als", pred)
+    params[dataset]["als"] = str(recommendation.kwargs)
+    print("als done")
+
+    parametrization = ng.p.Instrumentation(
+        regularization=reg, factors=factors, learning_rate=lr
+    )
+
+    optimizer = ng.optimizers.OnePlusOne(
+        parametrization=parametrization, budget=budget
+    )
+    recommendation = optimizer.minimize(train_bpr)
+    model = BayesianPersonalizedRanking(**recommendation.kwargs)
+    model = ImplicitWrap(model)
+    pred = model.fit_predict(train, k=max(k))
+    e.add_result("bpr", pred)
+    params[dataset]["bpr"] = str(recommendation.kwargs)
+    print("bpr done")
+
+    model = PopRec()
+    pred = model.fit_predict(train, max(k))
+    e.add_result("pop", pred)
+    print("pop done")
+
+    e.results.to_csv(f"implicit_{dataset}.csv")
+    print(f"time elapsed: {str(datetime.now() - start)}")
+
+with open("params.txt", "w") as f:
+    f.write(str(params))
+
+print(f"finished in: {str(datetime.now() - start)}")
