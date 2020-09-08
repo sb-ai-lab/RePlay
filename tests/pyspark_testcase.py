568e2f0f9461e76e950e6b3db2b148260b5f23c3
18235210 <yytamm@sberbank.ru>
2020-09-08 17:53:52 +0300

    Вынес функцию сравнения спарк датафреймов

--- tests/pyspark_testcase.py
+++ tests/pyspark_testcase.py
@@ -14,38 +14,40 @@ from pyspark.sql import DataFrame, SparkSession
 from replay.session_handler import State
 
 
+def unify_dataframe(data_frame: DataFrame):
+    pandas_df = data_frame.toPandas()
+    columns_to_sort_by: List[str] = []
+
+    if len(pandas_df) == 0:
+        columns_to_sort_by = pandas_df.columns
+    else:
+        for column in pandas_df.columns:
+            if not type(pandas_df[column][0]) in {
+                DenseVector,
+                list,
+                np.ndarray,
+            }:
+                columns_to_sort_by.append(column)
+
+    return (
+        pandas_df[sorted(data_frame.columns)]
+            .sort_values(by=sorted(columns_to_sort_by))
+            .reset_index(drop=True)
+    )
+
+def sparkDataFrameEqual(df1: DataFrame, df2: DataFrame):
+    return pd.testing.assert_frame_equal(
+        unify_dataframe(df1), unify_dataframe(df2), check_like=True
+    )
+
+
 class PySparkTest(unittest.TestCase):
-
     spark: SparkSession
-
     def assertSparkDataFrameEqual(
         self, df1: DataFrame, df2: DataFrame, msg: Optional[str] = None
     ) -> None:
-        def _unify_dataframe(data_frame: DataFrame):
-            pandas_df = data_frame.toPandas()
-            columns_to_sort_by: List[str] = []
-
-            if len(pandas_df) == 0:
-                columns_to_sort_by = pandas_df.columns
-            else:
-                for column in pandas_df.columns:
-                    if not type(pandas_df[column][0]) in {
-                        DenseVector,
-                        list,
-                        np.ndarray,
-                    }:
-                        columns_to_sort_by.append(column)
-
-            return (
-                pandas_df[sorted(data_frame.columns)]
-                .sort_values(by=sorted(columns_to_sort_by))
-                .reset_index(drop=True)
-            )
-
         try:
-            pd.testing.assert_frame_equal(
-                _unify_dataframe(df1), _unify_dataframe(df2), check_like=True
-            )
+            sparkDataFrameEqual(df1, df2)
         except AssertionError as e:
             raise self.failureException(msg) from e
 
