508785bba8df9d4b14b69b6cd8e116d4474d0679
18235210 <yytamm@sberbank.ru>
2020-09-08 17:07:10 +0300

    тест на проверку того, что оно не падает

---
+++ tests/models/test_stack.py
@@ -0,0 +1,25 @@
+# type: ignore
+# pylint: disable-all
+
+import pandas as pd
+import pytest
+
+from replay.models import KNN, Stack
+
+
+@pytest.fixture
+def df():
+    df = pd.DataFrame(
+        {
+            "user_id": [1, 1, 2, 2, 1, 2, 1, 2, 3, 3, 3, 4, 4, 4],
+            "item_id": [1, 2, 2, 3, 3, 4, 5, 7, 7, 5, 3, 4, 2, 5],
+            "relevance": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        }
+    )
+    return df
+
+
+def test_finishes(df):
+    stack = Stack([KNN()], n_folds=2)
+    stack.fit_predict(df, 1)
+    assert True
