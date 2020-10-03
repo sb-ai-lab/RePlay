fe4607d65aaa13e130972b473c14d6534dec7711
18235210 <yytamm@sberbank.ru>
2020-10-03 21:37:59 +0300

    old poetry

--- build_package.sh
+++ build_package.sh
@@ -5,7 +5,7 @@ python3 -m venv venv
 source ./venv/bin/activate
 # TODO: pypandoc нужен для pyspark, cython для implicit но почему-то некорректно взаимодействуют с poetry
 pip install --index-url http://mirror.sigma.sbrf.ru/pypi/simple \
-    --trusted-host mirror.sigma.sbrf.ru -U poetry pip pypandoc cython
+    --trusted-host mirror.sigma.sbrf.ru -U poetry==1.0.5 pip pypandoc cython
 # TODO: добавить сборку whl и выкладывания в Nexus
 poetry lock
 poetry install
