e09a61cc01c0b226feb2c2a0234f153e70769c85
Володкевич Анна Алексеевна <AAVolodkevich@sberbank.ru>
2020-11-23 11:49:07 +0300

    black

--- replay/models/base_rec.py
+++ replay/models/base_rec.py
@@ -27,7 +27,9 @@ class BaseRecommender(ABC):
     _logger: Optional[logging.Logger] = None
     can_predict_cold_users: bool = False
     can_predict_cold_items: bool = False
-    _search_space: Dict[str, Union[str, Sequence[Union[str, int, float]]]] = dict()
+    _search_space: Dict[
+        str, Union[str, Sequence[Union[str, int, float]]]
+    ] = dict()
 
     def set_params(self, **params: Dict[str, Any]) -> None:
         """
