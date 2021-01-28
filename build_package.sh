0399bdd19bd9fa1d61575239f3f3a6a9838631ca
18235210 <yytamm@sberbank.ru>
2021-01-28 16:08:46 +0300

    автоматически определяем, откуда ставить пакеты и редактируем .toml файл

--- build_package.sh
+++ build_package.sh
@@ -3,9 +3,10 @@
 # TODO: запросить у техподдержки Jenkins обновить Python (сейчас 3.6.8)
 python3 -m venv venv
 source ./venv/bin/activate
-# TODO: pypandoc нужен для pyspark, cython для implicit но почему-то некорректно взаимодействуют с poetry
-pip install --index-url http://mirror.sigma.sbrf.ru/pypi/simple \
-    --trusted-host mirror.sigma.sbrf.ru -U poetry pip pypandoc cython optuna
+
+# Прописываем откуда брать пакеты и устанавливаем то, что через поэтри не ставится
+python3 resolve_mirror.py
+
 # TODO: добавить сборку whl и выкладывания в Nexus
 poetry lock
 poetry install
---
+++ resolve_mirror.py
@@ -0,0 +1,43 @@
+# pylint: disable-all
+import urllib.request
+import urllib
+import os
+
+packages = "poetry pip pypandoc cython optuna"
+
+
+def fix():
+    net = "free"
+    try:
+        urllib.request.urlopen("http://mirror.ca.sbrf.ru")
+        net = "ca"
+    except urllib.error.URLError:
+        pass
+
+    try:
+        urllib.request.urlopen("http://mirror.sigma.sbrf.ru")
+        net = "sigma"
+    except urllib.error.URLError:
+        pass
+
+    if net == "free":
+        os.system(f"pip install {packages}")
+    else:
+        url = f"http://mirror.{net}.sbrf.ru/pypi/simple"
+        host = f"mirror.{net}.sbrf.ru"
+        command = f"pip install --index-url {url} --trusted-host {host}  -U {packages}"
+        os.system(command)
+
+        with open("pyproject.toml", "a") as file:
+            file.write(
+                f"""
+[[tool.poetry.source]]
+name = "private-pypi"
+url = "http://mirror.{net}.sbrf.ru/pypi/simple/"
+default = true
+"""
+            )
+
+
+if __name__ == "__main__":
+    fix()
