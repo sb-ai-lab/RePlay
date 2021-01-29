56d31293d0856b52bc985d07c7929b444038657f
18235210 <yytamm@sberbank.ru>
2021-01-29 18:11:43 +0300

    чтобы использовать пакет нужно его вначале установить =\

--- build_package.sh
+++ build_package.sh
@@ -3,6 +3,7 @@
 # TODO: запросить у техподдержки Jenkins обновить Python (сейчас 3.6.8)
 python3 -m venv venv
 source ./venv/bin/activate
+pip install toml
 
 # Прописываем откуда брать пакеты и устанавливаем то, что через поэтри не ставится
 python3 resolve_mirror.py
