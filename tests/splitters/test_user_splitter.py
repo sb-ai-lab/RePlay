47b31e8186d877316c69fb3f6c34d39fd2e07ce3
18235210 <yytamm@sberbank.ru>
2021-02-11 13:49:27 +0300

    закончил с UserSplitter, удалил файл с юниттестами

--- tests/splitters/test_user_splitter.py
+++ tests/splitters/test_user_splitter.py
@@ -44,7 +44,7 @@ def test_get_test_users(log, fraction):
 
 
 @pytest.mark.parametrize("fraction", [5, 1.0])
-def test_exceptions(log, fraction):
+def test_user_test_size_exception(log, fraction):
     splitter = UserSplitter(
         drop_cold_items=False,
         drop_cold_users=False,
@@ -53,3 +53,109 @@ def test_exceptions(log, fraction):
     )
     with pytest.raises(ValueError):
         splitter._get_test_users(log)
+
+
+@pytest.fixture
+def big_log(spark):
+    return spark.createDataFrame(
+        data=[
+            ["user1", "item4", datetime(2019, 9, 12), 1.0],
+            ["user1", "item5", datetime(2019, 9, 13), 2.0],
+            ["user1", "item7", datetime(2019, 9, 17), 1.0],
+            ["user1", "item6", datetime(2019, 9, 17), 1.0],
+            ["user2", "item4", datetime(2019, 9, 12), 1.0],
+            ["user2", "item5", datetime(2019, 9, 13), 2.0],
+            ["user2", "item6", datetime(2019, 9, 14), 3.0],
+            ["user2", "item2", datetime(2019, 9, 15), 4.0],
+            ["user2", "item3", datetime(2019, 9, 15), 4.0],
+            ["user3", "item4", datetime(2019, 9, 12), 1.0],
+            ["user3", "item5", datetime(2019, 9, 13), 2.0],
+            ["user3", "item6", datetime(2019, 9, 14), 3.0],
+            ["user3", "item2", datetime(2019, 9, 14), 3.0],
+            ["user3", "item7", datetime(2019, 9, 17), 1.0],
+            ["user4", "item2", datetime(2019, 9, 15), 4.0],
+            ["user4", "item1", datetime(2019, 9, 16), 4.0],
+            ["user4", "item4", datetime(2019, 9, 17), 4.0],
+            ["user4", "item5", datetime(2019, 9, 18), 4.0],
+            ["user4", "item8", datetime(2019, 9, 19), 4.0],
+            ["user4", "item4", datetime(2019, 9, 20), 4.0],
+            ["user4", "item1", datetime(2019, 9, 21), 4.0],
+        ],
+        schema=LOG_SCHEMA,
+    )
+
+
+test_sizes = np.arange(0.1, 1, 0.25).tolist() + list(range(1, 5))
+
+
+@pytest.mark.parametrize("item_test_size", test_sizes)
+def test_random_split(big_log, item_test_size):
+    splitter = UserSplitter(
+        drop_cold_items=False,
+        drop_cold_users=False,
+        item_test_size=item_test_size,
+        seed=1234,
+    )
+    train, test = splitter.split(big_log)
+    assert train.count() + test.count() == big_log.count()
+    assert test.intersect(train).count() == 0
+
+    if isinstance(item_test_size, int):
+        #  это грубая проверка; чтобы она была верна, необходимо
+        # чтобы item_test_size был больше длины лога каждого пользователя
+        num_users = big_log.select("user_id").distinct().count()
+        assert num_users * item_test_size == test.count()
+        assert big_log.count() - num_users * item_test_size == train.count()
+
+
+@pytest.mark.parametrize("item_test_size", [2.0, -1, -50, 2.1, -0.01])
+def test_item_test_size_exception(big_log, item_test_size):
+    splitter = UserSplitter(
+        drop_cold_items=False,
+        drop_cold_users=False,
+        item_test_size=item_test_size,
+        seed=1234,
+    )
+    with pytest.raises(ValueError):
+        splitter.split(big_log)
+
+
+@pytest.fixture
+def log2(spark):
+    return spark.createDataFrame(
+        data=[
+            ["1", "1", datetime(2019, 1, 1), 1.0],
+            ["1", "2", datetime(2019, 1, 2), 1.0],
+            ["1", "3", datetime(2019, 1, 3), 1.0],
+            ["1", "4", datetime(2019, 1, 4), 1.0],
+            ["2", "0", datetime(2020, 2, 5), 1.0],
+            ["2", "4", datetime(2020, 2, 4), 1.0],
+            ["2", "3", datetime(2020, 2, 3), 1.0],
+            ["2", "2", datetime(2020, 2, 2), 1.0],
+            ["2", "1", datetime(2020, 2, 1), 1.0],
+            ["3", "1", datetime(1995, 1, 1), 1.0],
+            ["3", "2", datetime(1995, 1, 2), 1.0],
+            ["3", "3", datetime(1995, 1, 3), 1.0],
+        ],
+        schema=LOG_SCHEMA,
+    )
+
+
+def test_split_quantity(log2):
+    splitter = UserSplitter(
+        drop_cold_items=False, drop_cold_users=False, item_test_size=2,
+    )
+    train, test = splitter.split(log2)
+    num_items = test.toPandas().user_id.value_counts()
+    assert num_items.nunique() == 1
+    assert num_items.unique()[0] == 2
+
+
+def test_split_proportion(log2):
+    splitter = UserSplitter(
+        drop_cold_items=False, drop_cold_users=False, item_test_size=0.4,
+    )
+    train, test = splitter.split(log2)
+    num_items = test.toPandas().user_id.value_counts()
+    assert num_items["2"] == 2
+    assert num_items["1"] == 1 and num_items["3"] == 1
