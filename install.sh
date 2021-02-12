3f5aee71fb5f90c57a5746bb1e54d4fd5442aabf
18330005 <avavasilyev@sigma.sbrf.ru>
2021-02-12 12:53:44 +0300

    внес исправления

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ python3 -m venv venv
 source ./venv/bin/activate
 # Прописываем откуда брать пакеты и возвращаем строку установки того, что через
 # поэтри не  ставится
-install_command = $(python3 resolve_mirror.py)
+install_command=$(python3 resolve_mirror.py)
 eval $install_command
 # TODO: добавить сборку whl и выкладывания в Nexus
 poetry lock
