ff070ea0c8272659b11ebe14b187a8b6c3aacdbe
18235210 <yytamm@sberbank.ru>
2021-02-17 18:59:22 +0300

    ситезированные пользователи

--- tests/splitters/test_cold_user_randrom_splitter.py
+++ tests/splitters/test_cold_user_randrom_splitter.py
@@ -2,33 +2,33 @@
 from datetime import datetime
 
 import pytest
+import numpy as np
 
-from replay.constants import LOG_SCHEMA
 from replay.splitters import ColdUserRandomSplitter
-from tests.utils import spark
 
 
 @pytest.fixture
-def log(spark):
-    return spark.createDataFrame(
-        data=[
-            ["user2", "item4", datetime(2019, 9, 14), 3.0],
-            ["user2", "item1", datetime(2019, 9, 14), 3.0],
-            ["user2", "item2", datetime(2019, 9, 15), 4.0],
-            ["user1", "item4", datetime(2019, 9, 12), 1.0],
-            ["user4", "item1", datetime(2019, 9, 12), 1.0],
-            ["user4", "item2", datetime(2019, 9, 13), 2.0],
-            ["user3", "item1", datetime(2019, 9, 16), 5.0],
-            ["user3", "item4", datetime(2019, 9, 16), 5.0],
-            ["user1", "item3", datetime(2019, 9, 17), 1.0],
-        ],
-        schema=LOG_SCHEMA,
-    ).repartition(1)
+def log():
+    import pandas as pd
+
+    return pd.DataFrame(
+        {
+            "user_id": list(range(5000)),
+            "item_id": list(range(5000)),
+            "relevance": [1] * 5000,
+        }
+    )
 
 
 def test(log):
-    cold_user_splitter = ColdUserRandomSplitter(1 / 4)
+    ratio = 0.25
+    cold_user_splitter = ColdUserRandomSplitter(ratio)
     cold_user_splitter.seed = 27
     train, test = cold_user_splitter.split(log)
     test_users = test.toPandas().user_id.unique()
-    assert len(test_users) == 2  # спарк плевать хотел на weights ¯\_(ツ)_/¯
+    train_users = train.toPandas().user_id.unique()
+    assert not np.isin(test_users, train_users).any()
+    real_ratio = len(test_users) / len(log)
+    assert np.isclose(
+        real_ratio, ratio, atol=0.01
+    )  # спарк плевать хотел на weights ¯\_(ツ)_/¯
