d0952b6b6700c291f93b4a38c1ab373ef19df738
monkey0head <volodkanna@ya.ru>
2022-07-19 14:58:46 +0300

    ucb optimize warning

--- replay/models/ucb.py
+++ replay/models/ucb.py
@@ -1,18 +1,19 @@
 import math
 
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 from pyspark.sql import DataFrame, Window
 from pyspark.sql import functions as sf
 
+from replay.metrics import Metric, NDCG
 from replay.models.base_rec import Recommender
 
 
 class UCB(Recommender):
-    """Calculates upper confidence bound
+    """Simple bandit model, which caclulate item relevance as upper confidence bound
     (`UCB <https://medium.com/analytics-vidhya/multi-armed-bandit-analysis-of-upper-confidence-bound-algorithm-4b84be516047>`_)
-    for the confidence interval
-    of true fraction of positive ratings.
+    for the confidence interval of true fraction of positive ratings.
+    Should be used in iterative (online) mode to achive preper recommendation quality.
 
     ``relevance`` from log must be converted to binary 0-1 form.
 
@@ -20,6 +21,9 @@ class UCB(Recommender):
         pred_i = ctr_i + \\sqrt{\\frac{c\\ln{n}}{n_i}}
 
     :math:`pred_i` -- predicted relevance of item :math:`i`
+    :math:`c` -- exploration coeficient
+    :math:`n` -- number of interactions in log
+    :math:`n_i` -- number of interactions with item :math:`i`
 
     >>> import pandas as pd
     >>> data_frame = pd.DataFrame({"user_idx": [1, 2, 3, 3], "item_idx": [1, 2, 1, 2], "relevance": [1, 0, 0, 0]})
@@ -46,20 +50,55 @@ class UCB(Recommender):
     item_popularity: DataFrame
     fill: float
 
-    def __init__(self, coef=2):
+    def __init__(self, exploration_coef=2):
         """
-        :param coef: exploration coefficient
+        :param exploration_coef: exploration coefficient
         """
         # pylint: disable=super-init-not-called
-        self.coef = coef
+        self.coef = exploration_coef
 
     @property
     def _init_args(self):
-        return {"coef": self.coef}
+        return {"exploration_coef": self.coef}
 
     @property
     def _dataframes(self):
         return {"item_popularity": self.item_popularity}
+
+    # pylint: disable=too-many-arguments
+    def optimize(
+        self,
+        train: DataFrame,
+        test: DataFrame,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+        param_borders: Optional[Dict[str, List[Any]]] = None,
+        criterion: Metric = NDCG(),
+        k: int = 10,
+        budget: int = 10,
+        new_study: bool = True,
+    ) -> None:
+        """
+        Searches best parameters with optuna.
+
+        :param train: train data
+        :param test: test data
+        :param user_features: user features
+        :param item_features: item features
+        :param param_borders: a dictionary with search borders, where
+            key is the parameter name and value is the range of possible values
+            ``{param: [low, high]}``. In case of categorical parameters it is
+            all possible values: ``{cat_param: [cat_1, cat_2, cat_3]}``.
+        :param criterion: metric to use for optimization
+        :param k: recommendation list length
+        :param budget: number of points to try
+        :param new_study: keep searching with previous study or start a new study
+        :return: dictionary with best parameters
+        """
+        self.logger.warning(
+            "The UCB model has only exploration coefficient parameter, "
+            "which cannot not be directly optimized"
+        )
 
     def _fit(
         self,
@@ -93,10 +132,6 @@ class UCB(Recommender):
         self.item_popularity.cache().count()
 
         self.fill = 1 + math.sqrt(math.log(self.coef * full_count))
-
-    @property
-    def _dataframes(self):
-        return {"item_popularity": self.item_popularity}
 
     def _clear_cache(self):
         if hasattr(self, "item_popularity"):
