2ddb4bd32ba79f250ba3e7e5111ac3ec1b6ae5ad
Petr Kuderov <pkuderov@gmail.com>
2022-09-05 15:02:03 +0300

    polish test srcipt: use rs_datasets, support spark and experiment run configs

--- experiments/04_cql_compare.py
+++ experiments/04_cql_compare.py
@@ -1,8 +1,12 @@
 import logging
+import os
 import time
 import warnings
+from argparse import ArgumentParser
+from math import floor
 
 import pandas as pd
+import psutil
 import torch
 import tqdm
 from optuna.exceptions import ExperimentalWarning
@@ -11,13 +15,16 @@ from pyspark.sql import functions as sf
 from replay.data_preparator import DataPreparator, Indexer
 from replay.experiment import Experiment
 from replay.metrics import HitRate, NDCG, MAP, MRR, Coverage, Surprisal
-from replay.models import ALSWrap, KNN, LightFMWrap, SLIM, UCB, CQL, Wilson
-from replay.session_handler import State
+from replay.models import ALSWrap, ItemKNN, LightFMWrap, SLIM, UCB, CQL, Wilson, Recommender
+from replay.session_handler import State, get_spark_session
 from replay.splitters import DateSplitter
 from replay.utils import get_log_info
 
 
-def fit_predict_add_res(name, model, experiment, train, top_k, test_users):
+def fit_predict_add_res(
+        name: str, model: Recommender, experiment: Experiment,
+        train: pd.DataFrame, top_k: int, test_users: pd.DataFrame
+):
     """
     Run fit_predict for the `model`, measure time on fit_predict and evaluate metrics
     """
@@ -41,22 +48,51 @@ def fit_predict_add_res(name, model, experiment, train, top_k, test_users):
     pred.unpersist()
 
 
-if __name__ == '__main__':
+def get_dataset(ds_name: str) -> tuple[pd.DataFrame, dict[str, str]]:
+    ds_name, category = ds_name.split('.')
+
+    if ds_name == 'MovieLens':
+        from rs_datasets import MovieLens
+        ml = MovieLens(category)
+    elif ds_name == 'Amazon':
+        from rs_datasets import Amazon
+        ml = Amazon(category=category)
+    else:
+        raise KeyError()
+
+    col_mapping = {
+        'user_id': 'user_id',
+        'item_id': 'item_id',
+        'relevance': 'rating',
+        'timestamp': 'timestamp'
+    }
+    return ml.ratings, col_mapping
+
+
+def main():
     warnings.filterwarnings("ignore", category=UserWarning)
     warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
     use_gpu = torch.cuda.is_available()
 
-    spark = State().session
+    parser = ArgumentParser()
+    parser.add_argument('--ds', dest='dataset', required=False, default='MovieLens.100k')
+    parser.add_argument('--epochs', dest='epochs', nargs='*', type=int, required=True)
+    parser.add_argument('--part', dest='partitions', type=float, required=False, default=0.8)
+    parser.add_argument('--mem', dest='memory', type=float, required=False, default=0.7)
+
+    args = parser.parse_args()
+    ds = args.dataset
+    n_epochs: set[int] = set(args.epochs)
+
+    spark = get_spark_session(
+        spark_memory=floor(psutil.virtual_memory().total / 1024 ** 3 * args.memory),
+        shuffle_partitions=floor(os.cpu_count() * args.partitions),
+    )
+    spark = State(session=spark).session
     spark.sparkContext.setLogLevel('ERROR')
 
-    prefix = "./data/"
-    df_log = pd.read_csv(
-        f'{prefix}/ml1m_ratings.dat', sep='\t',
-        names=['user_id', 'item_id', 'relevance', 'timestamp']
-    )
-
-    col_mapping = {key: key for key in ['user_id', 'item_id', 'relevance', 'timestamp']}
+    df_log, col_mapping = get_dataset(ds)
 
     data_preparator = DataPreparator()
     log = data_preparator.transform(columns_mapping=col_mapping, data=df_log)
@@ -78,7 +114,7 @@ if __name__ == '__main__':
 
     # train/test split
     date_splitter = DateSplitter(
-        test_start=0.98,
+        test_start=0.2,
         drop_cold_items=True,
         drop_cold_users=True,
 
@@ -109,13 +145,17 @@ if __name__ == '__main__':
     })
 
     algorithms = {
-        'CQL': CQL(use_gpu=use_gpu, k=K, n_epochs=3),
-        'ALS': ALSWrap(seed=SEED),
-        'KNN': KNN(num_neighbours=K),
-        'LightFM': LightFMWrap(random_state=SEED),
-        'SLIM': SLIM(seed=SEED),
-        'UCB': UCB(exploration_coef=0.5)
+        f'CQL_{e}': CQL(use_gpu=use_gpu, k=K, n_epochs=e)
+        for e in n_epochs
     }
+
+    algorithms.update({
+        'ALS': ALSWrap(seed=SEED),
+        'KNN': ItemKNN(num_neighbours=K),
+        'LightFM': LightFMWrap(random_state=SEED),
+        # 'SLIM': SLIM(seed=SEED),
+        'UCB': UCB(exploration_coef=0.5)
+    })
 
     logger = logging.getLogger("replay")
     test_users = test.select('user_idx').distinct()
@@ -130,9 +170,16 @@ if __name__ == '__main__':
             train_ = pos_neg_train
         fit_predict_add_res(name, model, experiment, train=train_, top_k=K, test_users=test_users)
         print(
-            experiment.results[
-                ['NDCG@{}'.format(K), 'MRR@{}'.format(K), 'Coverage@{}'.format(K), 'fit_time']
-            ].sort_values('NDCG@{}'.format(K), ascending=False)
+            experiment.results[[
+                f'NDCG@{K}', f'MRR@{K}', f'Coverage@{K}', 'fit_time'
+            ]].sort_values(f'NDCG@{K}', ascending=False)
         )
 
+        results_md = experiment.results.sort_values(f'NDCG@{K}', ascending=False).to_markdown()
+        with open(f'{ds}.md', 'w') as text_file:
+            text_file.write(results_md)
+
+
+if __name__ == '__main__':
+    main()
 
