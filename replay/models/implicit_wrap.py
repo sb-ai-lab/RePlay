bcf6c9e1e07f8c81819a8d784cf97984d87ff9ef
kasougi <ishutovnikita@gmail.com>
2022-09-23 13:21:30 +0300

    New version

--- replay/models/implicit_wrap.py
+++ replay/models/implicit_wrap.py
@@ -54,7 +54,9 @@ class ImplicitWrap(Recommender):
         user_features: Optional[DataFrame] = None,
         item_features: Optional[DataFrame] = None,
     ) -> None:
-        matrix = to_csr(log).T
+        matrix = to_csr(log)
+        print(matrix[1,2] == 1)
+        self.csr_log = matrix
         self.model.fit(matrix)
 
     # pylint: disable=too-many-arguments
@@ -70,14 +72,14 @@ class ImplicitWrap(Recommender):
     ) -> DataFrame:
         def predict_by_user(pandas_df: pd.DataFrame) -> pd.DataFrame:
             user = int(pandas_df["user_idx"].iloc[0])
-            res = model.recommend(
-                user, user_item_data, k, filter_seen_items, items_to_drop
+            ids, rel = model.recommend(
+                user, user_item_data[user], k, filter_seen_items, items_to_drop
             )
             return pd.DataFrame(
                 {
-                    "user_idx": [user] * len(res),
-                    "item_idx": [val[0] for val in res],
-                    "relevance": [val[1] for val in res],
+                    "user_idx": [user] * len(ids),
+                    "item_idx": [val for val in ids],
+                    "relevance": [val for val in rel],
                 }
             )
 
@@ -95,3 +97,27 @@ class ImplicitWrap(Recommender):
             .groupby("user_idx")
             .applyInPandas(predict_by_user, REC_SCHEMA)
         )
+
+    def _predict_pairs(
+        self,
+        pairs: DataFrame,
+        log: Optional[DataFrame] = None,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+    ) -> DataFrame:
+
+        user_plays_pred = to_csr(log) if log else self.csr_log
+
+        users = pairs.select("user_idx").distinct().toPandas().user_idx.to_list()
+        items = pairs.select("item_idx").distinct().toPandas().item_idx.to_list()
+        item_grid, rel = self.model.recommend(
+            users, user_plays_pred[users], len(items), False, items=items #?
+        )
+        user_grid = np.repeat(np.array(users)[:, np.newaxis], len(items), axis=1)
+        res = np.stack([user_grid, item_grid, rel], axis=2).reshape(-1, 3)
+        res_df = pd.DataFrame(res)
+        res_df = res_df.astype(dtype={0: "int64",
+                                      1: "int64", 2: "float64"})
+        res_spark = spark.createDataFrame(res_df, ["user_idx", "item_idx", "relevance"])
+
+        return res_spark
