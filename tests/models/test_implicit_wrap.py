a99b1e7b87d49cd509882308d7ec07089870fe6d
kasougi <ishutovnikita@gmail.com>
2022-10-03 13:20:36 +0300

    Fix test

--- tests/models/test_implicit_wrap.py
+++ tests/models/test_implicit_wrap.py
@@ -11,30 +11,8 @@ from pyspark.sql.types import (
 
 from replay.constants import LOG_SCHEMA
 from replay.models import ImplicitWrap
-from tests.utils import spark
+from tests.utils import spark, log
 from replay.session_handler import get_spark_session
-
-
-@pytest.fixture
-def spark():
-    return get_spark_session(1, 1)
-
-
-@pytest.fixture
-def log(spark):
-    date = datetime(2019, 1, 1)
-    return spark.createDataFrame(
-        data=[
-            [0, 0, date, 1.0],
-            [1, 0, date, 1.0],
-            [2, 1, date, 2.0],
-            [2, 1, date, 2.0],
-            [1, 1, date, 2.0],
-            [2, 2, date, 2.0],
-            [0, 2, date, 2.0],
-        ],
-        schema=LOG_SCHEMA,
-    )
 
 
 @pytest.fixture
@@ -68,7 +46,7 @@ def test_predict(model, log):
         filter_seen_items=False
     )
     assert len(pred.toPandas()["user_idx"].unique()) == 1
-    assert pred.toPandas().shape[0] == 2
+    assert pred.count() == 2
 
 
 @pytest.mark.parametrize(
