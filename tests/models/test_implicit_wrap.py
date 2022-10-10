f8f0896fcf7096a0c209401489a1393f4c50cb35
kasougi <ishutovnikita@gmail.com>
2022-10-10 12:12:28 +0300

    fix

--- tests/models/test_implicit_wrap.py
+++ tests/models/test_implicit_wrap.py
@@ -25,26 +25,22 @@ test_models = [
 
 @pytest.mark.parametrize(
     "model",
-    [
-        ImplicitWrap(implicit.als.AlternatingLeastSquares()),
-        ImplicitWrap(implicit.bpr.BayesianPersonalizedRanking()),
-        ImplicitWrap(implicit.lmf.LogisticMatrixFactorization()),
-    ]
+    test_models
 )
 @pytest.mark.parametrize(
-    "filter_see",
+    "filter_seen",
     [
         True,
         False
     ]
 )
-def test_predict(model, log,filter_see):
+def test_predict(model, log,filter_seen):
     model.fit(log)
     pred = model.predict(
         log=log,
         k=5,
         users=[1],
-        filter_seen_items=filter_see
+        filter_seen_items=filter_seen
     )
 
     assert pred.select("user_idx").distinct().count() == 1
