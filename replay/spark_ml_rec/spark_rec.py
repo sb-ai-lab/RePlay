ea849c164fbb5afd348ab149a66b8efd086629bf
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-01-18 19:20:04 +0300

    added predictMode param for spark_rec

--- replay/spark_ml_rec/spark_rec.py
+++ replay/spark_ml_rec/spark_rec.py
@@ -30,6 +30,14 @@ class SparkRecModelParams(Params):
         typeConverter=TypeConverters.toString,
     )
 
+    predictMode = Param(
+        Params._dummy(),
+        "predictMode",
+        "defines to make recommendations for incoming unique users / items "
+        "or to make relevance estimations for incoming user/item pairs",
+        typeConverter=TypeConverters.toString
+    )
+
     def __init__(self):
         super(SparkRecModelParams, self).__init__()
         self._setDefault(numRecommendations=10, filterSeenItems=True)
@@ -46,6 +54,9 @@ class SparkRecModelParams(Params):
     def getRecsFilePath(self) -> Optional[str]:
         return self.getOrDefault(self.recsFilePath)
 
+    def getPredictMode(self) -> str:
+        return self.getOrDefault(self.predictMode)
+
     def setNumRecommendations(self, value: int):
         self.set(self.numRecommendations, value)
 
@@ -54,6 +65,11 @@ class SparkRecModelParams(Params):
 
     def setRecsFilePath(self, value: str):
         self.set(self.recsFilePath, value)
+
+    def setPredictMode(self, value: str):
+        assert value in ["recommendations", "pairs_relevance_estimating"]
+
+        self.set(self.predictMode, value)
 
 
 class SparkRecParams(SparkRecModelParams):
@@ -65,30 +81,43 @@ class SparkRecModel(SparkBaseRecModel, SparkRecModelParams):
                  model: Recommender,
                  num_recommendations: int = 10,
                  filter_seen_items: bool = True,
-                 recs_file_path: Optional[str] = None):
+                 recs_file_path: Optional[str] = None,
+                 predict_mode: str = "recommendations"):
         super().__init__()
         self._model = model
         self.setNumRecommendations(num_recommendations)
         self.setFilterSeenItems(filter_seen_items)
+
         if recs_file_path is not None:
             self.setRecsFilePath(recs_file_path)
 
-    def transform(self, log: DataFrame, params: Optional[ParamMap] = None) -> DataFrame:
-        return self._model.predict(
-            log=log,
-            k=(
-                params.get(self.numRecommendations, self.getNumRecommendations())
-                if params else self.getNumRecommendations()
-            ),
-            filter_seen_items=(
-                params.get(self.filterSeenItems, self.getFilterSeenItems())
-                if params else self.getFilterSeenItems()
-            ),
-            recs_file_path=(
-                params.get(self.recsFilePath, self.getRecsFilePath())
-                if params else self.getRecsFilePath()
+        self.setPredictMode(predict_mode)
+
+    def transform(self, dataset: DataFrame, params: Optional[ParamMap] = None) -> DataFrame:
+        if self.predictMode == "recommendations":
+            return self._model.predict(
+                log=dataset,
+                k=(
+                    params.get(self.numRecommendations, self.getNumRecommendations())
+                    if params else self.getNumRecommendations()
+                ),
+                filter_seen_items=(
+                    params.get(self.filterSeenItems, self.getFilterSeenItems())
+                    if params else self.getFilterSeenItems()
+                ),
+                recs_file_path=(
+                    params.get(self.recsFilePath, self.getRecsFilePath())
+                    if params else self.getRecsFilePath()
+                )
             )
-        )
+        else:
+            return self._model.predict_pairs(
+                pairs=dataset,
+                recs_file_path=(
+                    params.get(self.recsFilePath, self.getRecsFilePath())
+                    if params else self.getRecsFilePath()
+                )
+            )
 
     def predict(
             self,
@@ -122,5 +151,9 @@ class SparkRec(SparkBaseRec, SparkRecParams):
         model = cast(Recommender, self._model.copy())
         model.fit(log)
         return SparkRecModel(
-            model, self.getNumRecommendations(), self.getFilterSeenItems(), self.getRecsFilePath()
+            model,
+            self.getNumRecommendations(),
+            self.getFilterSeenItems(),
+            self.getRecsFilePath(),
+            self.getPredictMode()
         )
