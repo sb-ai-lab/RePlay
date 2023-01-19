5f4868de4714af3b72c866c6026e3db72678ff13
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-01-19 18:02:29 +0300

    added unfinished demo with the pipeline

--- replay/spark_ml_rec/demo.py
+++ replay/spark_ml_rec/demo.py
@@ -1,4 +1,4 @@
-from pyspark.ml import Pipeline
+from pyspark.ml import Pipeline, PipelineModel
 from pyspark.sql import SparkSession
 from rs_datasets import MovieLens
 
@@ -8,7 +8,7 @@ from replay.spark_ml_rec.data_preparator import SparkIndexer
 from replay.spark_ml_rec.spark_rec import SparkRec
 from replay.spark_ml_rec.spark_user_rec import SparkUserRec
 from replay.spark_ml_rec.splitter import SparkTrainTestSplitter
-from replay.splitters import UserSplitter
+from replay.splitters import UserSplitter, DateSplitter
 from replay.utils import convert2spark
 
 spark = SparkSession.builder.getOrCreate()
@@ -17,6 +17,8 @@ ds = MovieLens('100k')
 
 log = convert2spark(ds.ratings)
 user_features = convert2spark(ds.users)
+
+log_train, log_test = DateSplitter(test_start=0.2, date_col="timestamp")
 
 pipe = Pipeline([
     # TODO: set column mapping
@@ -28,20 +30,19 @@ pipe = Pipeline([
         models=[
             SparkRec(model=Word2VecRec()),
             SparkRec(model=ALSWrap()),
-            SparkUserRec(model=ClusterRec(), user_features=user_features)
+            SparkUserRec(model=ClusterRec(), user_features=user_features, transient_user_features=True)
         ]
     )
 ])
 
-model = pipe.fit(log)
+model: PipelineModel = pipe.fit(log_train)
 
 # TODO: extract final params and extract study from optimize
 
-# TODO: save model
-# model.save()
+rec_model_path = "/tmp/rec_pipe"
+model.write().save(rec_model_path)
+rec_model = PipelineModel.read().load(rec_model_path)
 
-# TODO: load model
-
-# TODO: send other params via pipeline ?
-recs = model.transform(log, params={"userFeatures": user_features, "k": 100})
+# TODO: is it correct to send such params this way?
+recs = rec_model.transform(log_test, params={"userFeatures": user_features, "k": 100, "filter_seen_items": True})
 recs.write.parquet("recs.parquet", mode='overwrite')
