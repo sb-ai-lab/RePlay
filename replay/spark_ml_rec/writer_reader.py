5c54d8eb0e499ad1d1b8e101cccdbe7f0b23f1f6
fonhorst <fonhorst@alipoov.nb@gmail.com>
2023-01-20 16:37:50 +0300

    ---

--- replay/spark_ml_rec/writer_reader.py
+++ replay/spark_ml_rec/writer_reader.py
@@ -10,20 +10,23 @@ class DataframeAwareDefaultParamsWriter(DefaultParamsWriter):
         # TODO: delete dataframe like entities from paramMap
         dfParamsMap = {p: value for p, value in self.instance._paramMap.items() if isinstance(value, DataFrame)}
         self.instance._paramMap = {p: value for p, value in self.instance._paramMap.items() if not isinstance(value, DataFrame)}
-        # defaultParamMap = self.instance._defaultParamMap
+        dfDefaultParamMap = {p: value for p, value in self.instance._defaultParamMap.items() if isinstance(value, DataFrame)}
+        self.instance._defaultParamMap = {p: value for p, value in self.instance._defaultParamMap.items() if not isinstance(value, DataFrame)}
 
         params_path = os.path.join(path, "params")
         dfs_path = os.path.join(path, "dataframes_params")
+        default_dfs_path = os.path.join(path, "dataframes_default_params")
 
         # save what is left
         super().saveImpl(params_path)
 
-        # TODO: save dataframe entities
         for p, df in dfParamsMap.items():
             df_path = os.path.join(dfs_path, p.name)
-            # TODO: handle overwriting
+            df.write.parquet(df_path)
+
+        for p, df in dfDefaultParamMap.items():
+            df_path = os.path.join(default_dfs_path, p.name)
             df.write.parquet(df_path)
 
         self.instance._paramMap = self.instance._paramMap.update(dfParamsMap)
-
-        # TODO: restore dataframe like entities to paramMap
+        self.instance._defaultParamMap = self.instance._defaultParamMap.update(dfDefaultParamMap)
