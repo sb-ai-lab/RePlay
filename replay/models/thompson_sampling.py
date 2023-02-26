0b13f998644dbada5d4cd74e185799908926b82c
Alexey Grishanov <grishanov.av@phystech.edu>
2023-02-26 22:46:49 +0000

    Add Thompson Sampling (without refit)

--- replay/models/thompson_sampling.py
+++ replay/models/thompson_sampling.py
@@ -1,13 +1,56 @@
-import math
+from typing import Optional
 
-from typing import Any, Dict, List, Optional
-
+import numpy as np
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as sf
 
-from replay.metrics import Metric, NDCG
 from replay.models.base_rec import NonPersonalizedRecommender
 
 
-class Thompson(NonPersonalizedRecommender):
-    pass
+class ThompsonSampling(NonPersonalizedRecommender):
+    """
+    Thompson Sampling recommender.
+
+    """
+    def __init__(
+        self,
+        sample: bool = False,
+        seed: Optional[int] = None,
+    ):
+        self.sample = sample
+        self.seed = seed
+        super().__init__(add_cold_items=True, cold_weight=1)
+
+    @property
+    def _init_args(self):
+        return {"sample": self.sample, "seed": self.seed}
+
+    def _fit(
+        self,
+        log: DataFrame,
+        user_features: Optional[DataFrame] = None,
+        item_features: Optional[DataFrame] = None,
+    ) -> None:
+        self._check_relevance(log)
+
+        num_positive = log.filter(
+            log.relevance == sf.lit(1)
+        ).groupby("item_idx").agg(
+            sf.count("relevance").alias("positive")
+        )
+        num_negative = log.filter(
+            log.relevance == sf.lit(0)
+        ).groupby("item_idx").agg(
+            sf.count("relevance").alias("negative")
+        )
+
+        self.item_popularity = num_positive.join(
+            num_negative, how="inner", on="item_idx"
+        )
+
+        self.item_popularity = self.item_popularity.withColumn(
+            "relevance",
+            sf.udf(np.random.beta, "double")("positive", "negative")
+        ).drop("positive", "negative")
+        self.item_popularity.cache().count()
+        self.fill = np.random.beta(1, 1)
