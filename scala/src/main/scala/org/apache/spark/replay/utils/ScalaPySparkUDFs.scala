b6db2bb98e9c197583486ce89e5f9383d974c928
Azamat G <mr.g.azamat@gmail.com>
2023-03-17 01:39:37 +0300

    Fix problems

--- scala/src/main/scala/org/apache/spark/replay/utils/ScalaPySparkUDFs.scala
+++ scala/src/main/scala/org/apache/spark/replay/utils/ScalaPySparkUDFs.scala
@@ -1,15 +1,14 @@
 package org.apache.spark.replay.utils
 
-import org.apache.spark.sql.functions.udf
-import org.apache.spark.ml.linalg.DenseVector
-import org.apache.spark.ml.linalg.Vectors
 import breeze.numerics.log2
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions.udf
 
 object ScalaPySparkUDFs {
 
-    def _getMAPMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        val length = k.min(pred.size)
-        if (groundTruth.size == 0 || pred.size == 0) {
+    private def _getMAPMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        val length = k.min(pred.length)
+        if (groundTruth.length == 0 || pred.length == 0) {
             return 0
         }
         var tpCum = 0
@@ -20,26 +19,26 @@ object ScalaPySparkUDFs {
                 result = result + tpCum.toDouble / (i + 1)
             }
         }
-        return result / k
+        result / k
     }
 
-    val getMAPMetricValue = udf(_getMAPMetricValue _)
+    val getMAPMetricValue: UserDefinedFunction = udf(_getMAPMetricValue _)
 
-    def _getHitRateMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        val length = k.min(pred.size)
+    private def _getHitRateMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        val length = k.min(pred.length)
         for (i <- 0 until length) {
             if (groundTruth.contains(pred(i))) {
                 return 1
             }
         }
-        return 0
+        0
     }
 
-    val getHitRateMetricValue = udf(_getHitRateMetricValue _)
+    val getHitRateMetricValue: UserDefinedFunction = udf(_getHitRateMetricValue _)
 
-    def getNDCGMetricValue = udf { (k: Int, pred: Array[Int], groundTruth: Array[Int]) =>
-        val length = k.min(pred.size)
-        val groundTruthLen = k.min(groundTruth.size)
+    def getNDCGMetricValue: UserDefinedFunction = udf { (k: Int, pred: Array[Int], groundTruth: Array[Int]) =>
+        val length = k.min(pred.length)
+        val groundTruthLen = k.min(groundTruth.length)
         val denom:Array[Double] = (0 until k map(x => 1.0/log2(x + 2))).toArray
         var dcg = 0.0
         for (i <- 0 until length) {
@@ -54,9 +53,9 @@ object ScalaPySparkUDFs {
         dcg / idcg    
     }
 
-    def _getRocAucMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        val length = k.min(pred.size)
-        if (groundTruth.size == 0 || pred.size == 0) {
+    private def _getRocAucMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        val length = k.min(pred.length)
+        if (groundTruth.length == 0 || pred.length == 0) {
             return 0
         }
         var fpCur = 0
@@ -74,71 +73,71 @@ object ScalaPySparkUDFs {
         if (fpCum == 0) {
             return 1
         }
-        return 1.0 - fpCum.toDouble / (fpCur * (length - fpCur))
+        1.0 - fpCum.toDouble / (fpCur * (length - fpCur))
     }
 
-    val getRocAucMetricValue = udf(_getRocAucMetricValue _)
+    val getRocAucMetricValue: UserDefinedFunction = udf(_getRocAucMetricValue _)
 
-    def _getMRRMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        val length = k.min(pred.size)
+    private def _getMRRMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        val length = k.min(pred.length)
         for (i <- 0 until length) {
             if (groundTruth.contains(pred(i))) {
                 return 1.0 / (1.0 + i)
             }
         }
-        return 0
+        0
     }
 
-    val getMRRMetricValue = udf(_getMRRMetricValue _)
+    val getMRRMetricValue: UserDefinedFunction = udf(_getMRRMetricValue _)
 
-    def _getPrecisionMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        if (pred.size == 0) {
+    private def _getPrecisionMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        if (pred.length == 0) {
             return 0
         }
-        val length = k.min(pred.size)
+        val length = k.min(pred.length)
         val i = pred.take(length).toSet.intersect(groundTruth.toSet).size
-        return i.toDouble / k
+        i.toDouble / k
     }
 
-    val getPrecisionMetricValue = udf(_getPrecisionMetricValue _)
+    val getPrecisionMetricValue: UserDefinedFunction = udf(_getPrecisionMetricValue _)
 
-    def _getRecallMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
-        val length = k.min(pred.size)
+    private def _getRecallMetricValue(k: Int, pred: Array[Int], groundTruth: Array[Int]) : Double = {
+        val length = k.min(pred.length)
         val i = pred.take(length).toSet.intersect(groundTruth.toSet).size
-        return i.toDouble / groundTruth.size
+        i.toDouble / groundTruth.length
     }
 
-    val getRecallMetricValue = udf(_getRecallMetricValue _)
+    val getRecallMetricValue: UserDefinedFunction = udf(_getRecallMetricValue _)
 
-    def getSurprisalMetricValue = udf { (k: Int, weigths: Array[Double]) =>
+    def getSurprisalMetricValue: UserDefinedFunction = udf { (k: Int, weigths: Array[Double]) =>
         weigths.take(k).sum / k
     }
 
-    def _getUnexpectednessMetricValue(k: Int, pred: Array[Int], basePred: Array[Int]) : Double = {
-        if (pred.size == 0) {
+    private def _getUnexpectednessMetricValue(k: Int, pred: Array[Int], basePred: Array[Int]) : Double = {
+        if (pred.length == 0) {
             return 0
         }
-        return 1.0 - ( pred.take(k).toSet.intersect(basePred.take(k).toSet).size.toDouble / k )
+        1.0 - ( pred.take(k).toSet.intersect(basePred.take(k).toSet).size.toDouble / k )
     }
 
-    val getUnexpectednessMetricValue = udf(_getUnexpectednessMetricValue _)
+    val getUnexpectednessMetricValue: UserDefinedFunction = udf(_getUnexpectednessMetricValue _)
 
-    def _getNCISPrecisionMetricValue(k: Int, pred: Array[Int], predWeights: Array[Double], groundTruth: Array[Int]) : Double = {
-        if (pred.size == 0 || predWeights.size == 0) {
+    private def _getNCISPrecisionMetricValue(k: Int, pred: Array[Int], predWeights: Array[Double], groundTruth: Array[Int]) : Double = {
+        if (pred.length == 0 || predWeights.length == 0) {
             return 0
         }
-        val length = k.min(pred.size)
+        val length = k.min(pred.length)
         var sum1 = 0.0
         for (i <- 0 until length) {
             if (groundTruth.contains(pred(i))) {
-                if (i < predWeights.size) {
+                if (i < predWeights.length) {
                     sum1 += predWeights(i)
                 }
             }
         }
-        return sum1 / predWeights.take(k).sum
+        sum1 / predWeights.take(k).sum
     }
 
-    val getNCISPrecisionMetricValue = udf(_getNCISPrecisionMetricValue _)
+    val getNCISPrecisionMetricValue: UserDefinedFunction = udf(_getNCISPrecisionMetricValue _)
 
 }
