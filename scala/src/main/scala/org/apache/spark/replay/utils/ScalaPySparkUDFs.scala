a55bf66f4951f64a6504d3d4af5228934f8acb68
Azamat G <mr.g.azamat@gmail.com>
2023-03-18 00:35:42 +0300

    Fix problems

--- scala/src/main/scala/org/apache/spark/replay/utils/ScalaPySparkUDFs.scala
+++ scala/src/main/scala/org/apache/spark/replay/utils/ScalaPySparkUDFs.scala
@@ -3,11 +3,11 @@ package org.apache.spark.replay.utils
 import org.apache.spark.sql.functions.udf
 import org.apache.spark.ml.linalg.DenseVector
 import org.apache.spark.ml.linalg.Vectors
-import breeze.numerics.log2
+import org.apache.spark.sql.expressions.UserDefinedFunction
 
 object ScalaPySparkUDFs {
 
-    def multiplyUDF = udf { (scalar: Double, vector: DenseVector) =>
+    def multiplyUDF: UserDefinedFunction = udf { (scalar: Double, vector: DenseVector) =>
         val resultVector = new Array[Double](vector.size)
         vector.foreachActive {(index: Int, value: Double) => 
             resultVector(index) = scalar*value
