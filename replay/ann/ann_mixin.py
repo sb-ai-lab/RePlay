724a14917df9f5eb3815bbbea548610b9194eca5
Azamat G <mr.g.azamat@gmail.com>
2023-03-24 13:41:30 +0300

    Add docstrings to ANNMixin methods

--- replay/ann/ann_mixin.py
+++ replay/ann/ann_mixin.py
@@ -12,14 +12,16 @@ from replay.models.base_rec import BaseRecommender
 class ANNMixin(BaseRecommender):
     """
     This class overrides the `_fit_wrap` and `_inner_predict_wrap` methods of the base class,
-    adding an index construction in the `_fit_wrap` step and an index inference in the `_inner_predict_wrap` step.
+    adding an index construction in the `_fit_wrap` step
+    and an index inference in the `_inner_predict_wrap` step.
     """
 
     @cached_property
     def _spark_index_file_uid(self) -> str:
         """
         Cached property that returns the uuid for the index file name.
-        The unique name is needed to store the index file in `SparkFiles` without conflicts with other index files.
+        The unique name is needed to store the index file in `SparkFiles`
+        without conflicts with other index files.
         """
         return uuid.uuid4().hex[-12:]
 
@@ -28,16 +30,39 @@ class ANNMixin(BaseRecommender):
     def _use_ann(self) -> bool:
         """
         Property that determines whether the ANN (index) is used.
+        If `True`, then the index will be built (at the `fit` stage)
+        and index will be inferred (at the `predict` stage).
         """
-        ...
 
     @abstractmethod
     def _get_vectors_to_build_ann(self, log: DataFrame) -> DataFrame:
-        ...
+        """Implementations of this method must return a dataframe with item vectors.
+        Item vectors from this method are used to build the index.
+
+        Args:
+            log: DataFrame with interactions
+
+        Returns: DataFrame[item_idx int, vector array<double>] or DataFrame[vector array<double>].
+        Column names in dataframe can be anything.
+        """
 
     @abstractmethod
     def _get_ann_build_params(self, log: DataFrame) -> Dict[str, Any]:
-        ...
+        """Implementation of this method must return dictionary
+        with arguments for `_build_ann_index` method.
+
+        Args:
+            log: DataFrame with interactions
+
+        Returns: Dictionary with arguments to build index. For example: {
+            "id_col": "item_idx",
+            "features_col": "item_factors",
+            "params": self._hnswlib_params,
+            "dim": 100,
+            "num_elements": 10_000,
+        }
+
+        """
 
     def _fit_wrap(
         self,
@@ -45,6 +70,17 @@ class ANNMixin(BaseRecommender):
         user_features: Optional[DataFrame] = None,
         item_features: Optional[DataFrame] = None,
     ) -> None:
+        """Wrapper extends `_fit_wrap`, adds construction of ANN index by flag.
+
+        Args:
+            log: historical log of interactions
+                ``[user_idx, item_idx, timestamp, relevance]``
+            user_features: user features
+                ``[user_idx, timestamp]`` + feature columns
+            item_features: item features
+                ``[item_idx, timestamp]`` + feature columns
+
+        """
         super()._fit_wrap(log, user_features, item_features)
 
         if self._use_ann:
@@ -56,12 +92,31 @@ class ANNMixin(BaseRecommender):
     def _get_vectors_to_infer_ann_inner(
         self, log: DataFrame, users: DataFrame
     ) -> DataFrame:
-        ...
+        """Implementations of this method must return a dataframe with user vectors.
+        User vectors from this method are used to infer the index.
+
+        Args:
+            log: DataFrame with interactions
+            users: DataFrame with users
+
+        Returns: DataFrame[user_idx int, vector array<double>] or DataFrame[vector array<double>].
+        Vector column name in dataframe can be anything.
+        """
 
     def _get_vectors_to_infer_ann(
         self, log: DataFrame, users: DataFrame, filter_seen_items: bool
     ) -> DataFrame:
+        """This method wraps `_get_vectors_to_infer_ann_inner`
+        and adds seen items to dataframe with user vectors by flag.
 
+        Args:
+            log: DataFrame with interactions
+            users: DataFrame with users
+            filter_seen_items: flag to remove seen items from recommendations based on ``log``.
+
+        Returns:
+
+        """
         users = self._get_vectors_to_infer_ann_inner(log, users)
 
         # here we add `seen_item_idxs` to filter the viewed items in UDFs (see infer_index)
@@ -76,10 +131,19 @@ class ANNMixin(BaseRecommender):
 
     @abstractmethod
     def _get_ann_infer_params(self) -> Dict[str, Any]:
-        ...
+        """Implementation of this method must return dictionary
+        with arguments for `_infer_ann_index` method.
+
+        Returns: Dictionary with arguments to infer index. For example: {
+            "features_col": "user_vector",
+            "params": self._hnswlib_params,
+            "index_dim": self.rank,
+        }
+
+        """
 
     @abstractmethod
-    def _build_ann_index(
+    def _build_ann_index(  # pylint: disable=too-many-arguments
         self,
         vectors: DataFrame,
         features_col: str,
@@ -90,10 +154,27 @@ class ANNMixin(BaseRecommender):
         index_type: str = None,
         items_count: Optional[int] = None,
     ) -> None:
-        ...
+        """The method implements the construction of the ANN index.
+
+        Args:
+            vectors: DataFrame with vectors to build index
+            features_col: Name of column from `vectors` dataframe
+                that contains vectors to build index
+            params: Index params
+            dim: length of vectors from `vectors` dataframe
+            num_elements: if `index_type` != "sparse" then
+                number of elements that will be stored in the index
+            id_col: Name of column that contains identifiers of vectors.
+                None if `vectors` dataframe have no id column.
+            index_type: "sparse" or "dense".
+                If None then `index_type`="dense".
+            items_count: if `index_type` == "sparse" then
+                `items_count` is dimension of sparse matrix, else None
+
+        """
 
     @abstractmethod
-    def _infer_ann_index(
+    def _infer_ann_index(  # pylint: disable=too-many-arguments
         self,
         vectors: DataFrame,
         features_col: str,
@@ -104,9 +185,24 @@ class ANNMixin(BaseRecommender):
         index_type: str = None,
         log: DataFrame = None,
     ) -> DataFrame:
-        ...
+        """The method implements the inference of the ANN index.
 
-    def _inner_predict_wrap(
+        Args:
+            vectors: Dataframe that contains vectors to inference
+            features_col: Column name from `vectors` dataframe that contains vectors to inference
+            params: Index params
+            k: Desired number of neighbour vectors for vectors from `vectors` dataframe
+            filter_seen_items: flag to filter seen items before output
+            index_dim: Dimension of vectors in index
+            index_type: "sparse" or "dense".
+                If None then `index_type`="dense".
+            log: DataFrame with interactions
+
+        Returns: DataFrame[user_idx int, item_idx int, relevance double]
+
+        """
+
+    def _inner_predict_wrap(  # pylint: disable=too-many-arguments
         self,
         log: DataFrame,
         k: int,
@@ -116,7 +212,7 @@ class ANNMixin(BaseRecommender):
         item_features: Optional[DataFrame] = None,
         filter_seen_items: bool = True,
     ) -> DataFrame:
-
+        """Override base `_inner_predict_wrap` and adds ANN inference by condition"""
         if self._use_ann:
             vectors = self._get_vectors_to_infer_ann(
                 log, users, filter_seen_items
@@ -140,7 +236,8 @@ class ANNMixin(BaseRecommender):
                 filter_seen_items,
             )
 
-    def _unpack_infer_struct(self, inference_result: DataFrame) -> DataFrame:
+    @staticmethod
+    def _unpack_infer_struct(inference_result: DataFrame) -> DataFrame:
         """Transforms input dataframe.
         Unpacks and explodes arrays from `neighbours` struct.
 
@@ -168,7 +265,8 @@ class ANNMixin(BaseRecommender):
             ).alias("zip_exp"),
         )
 
-        # Fix arrays_zip random behavior. It can return zip_exp.0 or zip_exp.item_idx in different machines
+        # Fix arrays_zip random behavior.
+        # It can return zip_exp.0 or zip_exp.item_idx in different machines.
         fields = res.schema["zip_exp"].jsonValue()["type"]["fields"]
         item_idx_field_name: str = fields[0]["name"]
         distance_field_name: str = fields[1]["name"]
