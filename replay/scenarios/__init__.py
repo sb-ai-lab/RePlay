d8a0bb0a0ac0019efe4130823b038920d161928a
mralexdmitriy <mr.alexdmitriy@mail.ru>
2023-05-31 19:16:51 +0300

    added OneStageScenario

--- replay/scenarios/__init__.py
+++ replay/scenarios/__init__.py
@@ -3,4 +3,6 @@ Scenarios are a series of actions for recommendations
 """
 from replay.scenarios.fallback import Fallback
 from replay.scenarios.basescenario import BaseScenario
+from replay.scenarios.one_stage import OneStageScenario, OneStageItem2ItemScenario, OneStageUser2ItemScenario
 from replay.scenarios.two_stages.two_stages_scenario import TwoStagesScenario
+# from replay.scenarios.autorecsys_scenario import AutoRecSysScenario
