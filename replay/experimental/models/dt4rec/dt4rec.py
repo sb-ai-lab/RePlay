0c8fc92263259745fc360083dc4648318e128df4
Alexey <grishanov.av@phystech.edu>
2024-01-11 22:11:35 +0000

    Fix codestyle

--- replay/experimental/models/dt4rec/dt4rec.py
+++ replay/experimental/models/dt4rec/dt4rec.py
@@ -30,15 +30,15 @@ if TORCH_AVAILABLE:
 class DT4Rec(Recommender):
     """
     Decision Transformer for Recommendations
-    
+
     General Idea:
         `Decision Transformer: Reinforcement Learning
         via Sequence Modeling <https://arxiv.org/pdf/2106.01345.pdf>`_.
-    
+
     Ideas for improvements:
         `User Retention-oriented Recommendation with Decision
         Transformer <https://arxiv.org/pdf/2303.06347.pdf>`_.
-    
+
     Also, some sources are listed in their respective classes
     """
 
