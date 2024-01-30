a9a32c53ad7acd134e458cabefdebfe9e365145d
Denis Kulandin <dskulandin@sberbank.ru>
2024-01-30 14:32:52 +0300

    init

--- replay/utils/distributions.py
+++ replay/utils/distributions.py
@@ -1,63 +1,10 @@
 """Distribution calculations"""
-import seaborn as sns
-
 from .types import PYSPARK_AVAILABLE, DataFrameLike, PandasDataFrame
 
 if PYSPARK_AVAILABLE:
     from pyspark.sql import functions as sf
 
     from replay.utils.spark_utils import convert2spark, get_top_k_recs, spark_to_pandas
-
-
-def plot_user_dist(
-    user_dist: PandasDataFrame, window: int = 1, title: str = ""
-):  # pragma: no cover
-    """
-    Plot mean metric value by the number of user ratings
-
-    :param user_dist: output of ``user_distribution`` method for a metric
-    :param window: the number of closest values to average for smoothing
-    :param title: plot title
-    :return: plot object
-    """
-    user_dist["smoothed"] = (
-        user_dist["value"].rolling(window, center=True).mean()
-    )
-    plot = sns.lineplot(x="count", y="smoothed", data=user_dist)
-    plot.set(
-        xlabel="# of ratings",
-        ylabel="smoothed value",
-        title=title,
-        xscale="log",
-    )
-    return plot
-
-
-def plot_item_dist(
-    item_dist: PandasDataFrame, palette: str = "magma", col: str = "rec_count"
-):  # pragma: no cover
-    """
-    Show the results of  ``item_distribution`` method
-
-    :param item_dist: PandasDataFrame
-    :param palette: colour scheme for seaborn
-    :param col: column to use for a plot
-    :return: plot
-    """
-    limits = list(range(len(item_dist), 0, -len(item_dist) // 10))[::-1]
-    values = [(item_dist.iloc[:limit][col]).sum() for limit in limits]
-    # pylint: disable=too-many-function-args
-    plot = sns.barplot(
-        list(range(1, 11)),
-        values / max(values),
-        palette=sns.color_palette(palette, 10),
-    )
-    plot.set(
-        xlabel="popularity decentile",
-        ylabel="proportion",
-        title="Popularity distribution",
-    )
-    return plot
 
 
 def item_distribution(
