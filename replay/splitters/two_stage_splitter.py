03759a5886bc95b3a33d8012f12fa4d5ec3abaf0
Denis Kulandin <dskulandin@sberbank.ru>
2024-07-23 14:51:23 +0300

    fix polars bc

--- replay/splitters/two_stage_splitter.py
+++ replay/splitters/two_stage_splitter.py
@@ -239,12 +239,12 @@ class TwoStageSplitter(Splitter):
         res = res.join(counts, on=self.first_divide_column, how="left")
         res = res.with_columns((pl.col("_row_num") / pl.col("count")).alias("_frac"))
         res = res.fill_null(False)
-
+        print(res.schema)
         train = res.filter((pl.col("_frac") > self.second_divide_size) | (~pl.col("is_test"))).drop(
-            "_rand", "_row_num", "count", "_frac", "is_test"
+            "_row_num", "count", "_frac", "is_test"
         )
         test = res.filter((pl.col("_frac") <= self.second_divide_size) & pl.col("is_test")).drop(
-            "_rand", "_row_num", "count", "_frac", "is_test"
+            "_row_num", "count", "_frac", "is_test"
         )
 
         return train, test
