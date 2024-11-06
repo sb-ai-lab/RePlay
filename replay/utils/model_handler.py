d8fb8a15bdc4cb0e2e90af4f42164b3536ef6d44
Ruslan Izmaylov <RNIzmaylov@sberbank.ru>
2024-11-06 14:38:56 +0300

    remove deprecation_warning duplication in model_handler (leave only in utils/types)

--- replay/utils/model_handler.py
+++ replay/utils/model_handler.py
@@ -1,11 +1,9 @@
-import functools
 import json
 import os
 import pickle
-import warnings
 from os.path import join
 from pathlib import Path
-from typing import Any, Callable, Optional, Union
+from typing import Union
 
 from replay.data.dataset_utils import DatasetLabelEncoder
 from replay.models import *
@@ -151,25 +149,3 @@ def load_encoder(path: Union[str, Path]) -> DatasetLabelEncoder:
         encoder = pickle.load(f)
 
     return encoder
-
-
-def deprecation_warning(message: Optional[str] = None) -> Callable[..., Any]:
-    """
-    Decorator that throws deprecation warnings.
-
-    :param message: message to deprecation warning without func name.
-    """
-    base_msg = "will be deprecated in future versions."
-
-    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
-        @functools.wraps(func)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            msg = f"{func.__qualname__} {message if message else base_msg}"
-            warnings.simplefilter("always", DeprecationWarning)  # turn off filter
-            warnings.warn(msg, category=DeprecationWarning, stacklevel=2)
-            warnings.simplefilter("default", DeprecationWarning)  # reset filter
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
