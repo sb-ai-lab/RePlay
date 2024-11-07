f552b44a9e5671adc3f4e9554234c6a435c2d4d3
Ruslan Izmaylov <RNIzmaylov@sberbank.ru>
2024-11-07 14:59:18 +0300

    remove one more decorator, update toml and lock files

--- replay/utils/common.py
+++ replay/utils/common.py
@@ -1,8 +1,6 @@
-import functools
-import inspect
 import json
 from pathlib import Path
-from typing import Any, Callable, Union
+from typing import Union
 
 from polars import from_pandas as pl_from_pandas
 
@@ -43,6 +41,7 @@ from replay.utils import (
     PolarsDataFrame,
     SparkDataFrame,
 )
+from replay.utils.decorators import check_if_dataframe
 from replay.utils.spark_utils import (
     convert2spark as pandas_to_spark,
     spark_to_pandas,
@@ -109,37 +108,6 @@ def load_from_replay(path: Union[str, Path], **kwargs) -> SavableObject:
     obj = obj_type.load(path, **kwargs)
 
     return obj
-
-
-def _check_if_dataframe(var: Any):
-    if not isinstance(var, (SparkDataFrame, PolarsDataFrame, PandasDataFrame)):
-        msg = f"Object of type {type(var)} is not a dataframe of known type (can be pandas|spark|polars)"
-        raise ValueError(msg)
-
-
-def check_if_dataframe(*args_to_check: str) -> Callable[..., Any]:
-    def decorator_func(func: Callable[..., Any]) -> Callable[..., Any]:
-        @functools.wraps(func)
-        def wrap_func(*args: Any, **kwargs: Any) -> Any:
-            extended_kwargs = {}
-            extended_kwargs.update(kwargs)
-            extended_kwargs.update(dict(zip(inspect.signature(func).parameters.keys(), args)))
-            # add default param values to dict with arguments
-            extended_kwargs.update(
-                {
-                    x.name: x.default
-                    for x in inspect.signature(func).parameters.values()
-                    if x.name not in extended_kwargs and x.default is not x.empty
-                }
-            )
-            vals_to_check = [extended_kwargs[_arg] for _arg in args_to_check]
-            for val in vals_to_check:
-                _check_if_dataframe(val)
-            return func(*args, **kwargs)
-
-        return wrap_func
-
-    return decorator_func
 
 
 @check_if_dataframe("data")
--- replay/utils/decorators.py
+++ replay/utils/decorators.py
@@ -1,6 +1,44 @@
 import functools
+import inspect
 import warnings
 from typing import Any, Callable, Optional
+
+from replay.utils import (
+    PandasDataFrame,
+    PolarsDataFrame,
+    SparkDataFrame,
+)
+
+
+def _check_if_dataframe(var: Any):
+    if not isinstance(var, (SparkDataFrame, PolarsDataFrame, PandasDataFrame)):
+        msg = f"Object of type {type(var)} is not a dataframe of known type (can be pandas|spark|polars)"
+        raise ValueError(msg)
+
+
+def check_if_dataframe(*args_to_check: str) -> Callable[..., Any]:
+    def decorator_func(func: Callable[..., Any]) -> Callable[..., Any]:
+        @functools.wraps(func)
+        def wrap_func(*args: Any, **kwargs: Any) -> Any:
+            extended_kwargs = {}
+            extended_kwargs.update(kwargs)
+            extended_kwargs.update(dict(zip(inspect.signature(func).parameters.keys(), args)))
+            # add default param values to dict with arguments
+            extended_kwargs.update(
+                {
+                    x.name: x.default
+                    for x in inspect.signature(func).parameters.values()
+                    if x.name not in extended_kwargs and x.default is not x.empty
+                }
+            )
+            vals_to_check = [extended_kwargs[_arg] for _arg in args_to_check]
+            for val in vals_to_check:
+                _check_if_dataframe(val)
+            return func(*args, **kwargs)
+
+        return wrap_func
+
+    return decorator_func
 
 
 def deprecation_warning(message: Optional[str] = None) -> Callable[..., Any]:
