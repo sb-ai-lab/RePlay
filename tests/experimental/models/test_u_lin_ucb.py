2da2328d691635c91b9f0e3624fd31d03ba1f6e7
Alexey <grishanov.av@phystech.edu>
2024-11-08 07:46:29 +0000

    Fix codestyle

--- tests/experimental/models/test_u_lin_ucb.py
+++ tests/experimental/models/test_u_lin_ucb.py
@@ -10,7 +10,9 @@ from replay.experimental.models import ULinUCB
 
 @pytest.fixture(scope="module")
 def log_u_lin_ucb(log2):
-    return log2.withColumn("relevance", sf.when(sf.col("relevance") > 3, 1).otherwise(0))
+    return log2.withColumn(
+        "relevance", sf.when(sf.col("relevance") > 3, 1).otherwise(0)
+    )
 
 
 @pytest.fixture(scope="module")
@@ -22,9 +24,9 @@ def user_features(spark):
 
 @pytest.fixture(scope="module")
 def item_features(spark):
-    return spark.createDataFrame([(0, 4.0, 5.0), (1, 5.0, 4.0), (2, 5.0, 1.0), (3, 0.0, 4.0)]).toDF(
-        "item_idx", "item_feature_1", "item_feature_2"
-    )
+    return spark.createDataFrame(
+        [(0, 4.0, 5.0), (1, 5.0, 4.0), (2, 5.0, 1.0), (3, 0.0, 4.0)]
+    ).toDF("item_idx", "item_feature_1", "item_feature_2")
 
 
 @pytest.fixture(scope="module")
@@ -40,7 +42,12 @@ def test_predict_empty_log(fitted_model, log_u_lin_ucb, user_features, item_feat
     empty_log = log_u_lin_ucb.limit(0)
     users = log_u_lin_ucb.select("user_idx").distinct()
     pred = fitted_model.predict(
-        empty_log, k=1, users=users, items=list(range(10)), user_features=user_features, item_features=item_features
+        empty_log,
+        k=1,
+        users=users,
+        items=list(range(10)),
+        user_features=user_features,
+        item_features=item_features,
     )
 
     assert pred.count() == users.count()
