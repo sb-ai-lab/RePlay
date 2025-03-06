2d45d3af2644bc8476dc1cc87f32502755340d52
Ruslan Izmaylov <lord.rik@yandex.ru>
2025-03-06 17:26:19 +0300

    fix linters

--- replay/models/nn/sequential/compiled/__init__.py
+++ replay/models/nn/sequential/compiled/__init__.py
@@ -5,6 +5,7 @@ try:
     from .sasrec_compiled import SasRecCompiled
 except ImportError:  # pragma: no cover
     import warnings
+
     warnings.warn(
         "You are trying to import CPU-optimized model for inference via OpenVINO, but"
         "you have no onnx or openvino packages in your virtual env. Import will not be done."
