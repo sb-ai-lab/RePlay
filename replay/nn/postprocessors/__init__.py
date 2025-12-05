7cc7769bc55183e69abde9f341d3746590712c57
Denis Kulandin <dskulandin@sberbank.ru>
2025-12-05 14:59:41 +0300

    fix import

--- replay/nn/postprocessors/__init__.py
+++ replay/nn/postprocessors/__init__.py
@@ -1,5 +1,1 @@
-from replay.utils import TORCH_AVAILABLE
-
-if TORCH_AVAILABLE:
-    from ._base import BasePostProcessor
-    from .postprocessors import RemoveSeenItems, SampleItems
+from ._base import BasePostProcessor
