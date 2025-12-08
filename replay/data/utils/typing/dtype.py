d393d25bb9cb399c64994dfd729620d1e3413ae2
VAGolubenko <vagolubenko@sberbank.ru>
2025-12-09 00:13:46 +0300

    Cleanup

--- replay/data/utils/typing/dtype.py
+++ replay/data/utils/typing/dtype.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-from typing import Any, Union
 
 import numpy as np
 import pyarrow as pa
@@ -58,55 +57,3 @@ def pyarrow_to_torch(dtype: pa.DataType) -> torch.dtype:
         return numpy_to_torch(np_dtype)
 
     return _pyarrow_to_torch(dtype)
-
-
-def error_message(dtype: Any) -> str:
-    return f"Unknown dtype: {dtype}."
-
-
-def to_numpy(dtype: Union[torch.dtype, pa.DataType]) -> np.dtype:
-    @lru_cache
-    def _to_numpy(dtype: Union[torch.dtype, pa.DataType]) -> np.dtype:
-        result: np.dtype
-        if isinstance(dtype, torch.dtype):
-            result = torch_to_numpy(dtype)
-        elif isinstance(dtype, pa.DataType):
-            result = pyarrow_to_numpy(dtype)
-        else:
-            msg: str = error_message(dtype)
-            raise TypeError(msg)
-        return result
-
-    return _to_numpy(dtype)
-
-
-def to_torch(dtype: Union[np.dtype, pa.DataType]) -> torch.dtype:
-    @lru_cache
-    def _to_torch(dtype: Union[np.dtype, pa.DataType]) -> torch.dtype:
-        result: torch.dtype
-        if isinstance(dtype, np.dtype):
-            result = numpy_to_torch(dtype)
-        elif isinstance(dtype, pa.DataType):
-            result = pyarrow_to_torch(dtype)
-        else:
-            msg: str = error_message(dtype)
-            raise TypeError(msg)
-        return result
-
-    return _to_torch(dtype)
-
-
-def to_pyarrow(dtype: Union[np.dtype, torch.dtype]) -> pa.DataType:
-    @lru_cache
-    def _to_pyarrow(dtype: Union[np.dtype, torch.dtype]) -> pa.DataType:
-        result: pa.DataType
-        if isinstance(dtype, np.dtype):
-            result = numpy_to_pyarrow(dtype)
-        elif isinstance(dtype, torch.dtype):
-            result = torch_to_pyarrow(dtype)
-        else:
-            msg: str = error_message(dtype)
-            raise TypeError(msg)
-        return result
-
-    return _to_pyarrow(dtype)
