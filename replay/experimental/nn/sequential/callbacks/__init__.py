974415b7d04de0f8adb3c66bf077faab5ca5d34e
VAGolubenko <vagolubenko@sberbank.ru>
2025-12-18 09:57:44 +0300

    Add sasrec example (with experimental interfaces for now)

---
+++ replay/experimental/nn/sequential/callbacks/__init__.py
@@ -0,0 +1,14 @@
+from .prediction_callbacks import (
+    PandasPredictionCallback,
+    PolarsPredictionCallback,
+    SparkPredictionCallback,
+    TorchPredictionCallback,
+)
+from .validation_callback import ValidationMetricsCallback
+
+__all__ = [
+    "PandasPredictionCallback"
+    "PolarsPredictionCallback"
+    "SparkPredictionCallback"
+    "ValidationMetricsCallback"
+]
