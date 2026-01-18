7946085e9607448f2bf7e065b0379c263882bfb9
Ekaterina Tsinovnik <ekaterinatsinovnik@gmail.com>
2026-01-19 02:28:53 +0300

    Fix import in tests

--- replay/nn/lightning/callbacks/__init__.py
+++ replay/nn/lightning/callbacks/__init__.py
@@ -1,13 +1,9 @@
-from replay.utils import PYSPARK_AVAILABLE
-
-if PYSPARK_AVAILABLE:
-    from .predictions_callbacks import SparkTopItemsCallback
-
 from .metrics_callbacks import ComputeMetricsCallback
 from .predictions_callbacks import (
     HiddenStatesCallback,
     PandasTopItemsCallback,
     PolarsTopItemsCallback,
+    SparkTopItemsCallback,
     TopItemsCallbackBase,
     TorchTopItemsCallback,
 )
