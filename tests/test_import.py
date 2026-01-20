6715da0c3bc28b26008fc411e164e4ed657580c2
Ekaterina Tsinovnik <ekaterinatsinovnik@gmail.com>
2026-01-20 16:57:11 +0300

    fix

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,18 @@
+import builtins
+
 import pytest
 
 
 @pytest.mark.core
-def test_import_nn_module():
+def test_import_nn_raises_if_torch_missing(monkeypatch):
+    real_import = builtins.__import__
+
+    def fake_import(name, args, *kwargs):
+        if name == "torch" or name.startswith("torch."):
+            raise ImportError
+        return real_import(name, args, *kwargs)
+
+    monkeypatch.setattr(builtins, "__import__", fake_import)
+
     with pytest.raises(ImportError):
         from replay import nn  # noqa: F401
