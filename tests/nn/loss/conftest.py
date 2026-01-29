51b681ee9577b5ba10fff2fe5c4859f60682b0e5
Denis Kulandin <dskulandin@sberbank.ru>
2026-01-29 12:06:34 +0300

    fix

--- tests/nn/loss/conftest.py
+++ tests/nn/loss/conftest.py
@@ -22,23 +22,6 @@ def hidden_simple_batch():
 
 
 @pytest.fixture
-<<<<<<< HEAD
-def hidden_simple_batch_multipositive():
-    seq_len = 6
-    num_multipositives = 5
-    positive_labels = torch.LongTensor([[[3], [3], [2], [0], [1], [2]]]).repeat(1, 1, num_multipositives)
-    return {
-        "model_embeddings": torch.rand(1, seq_len, 32),
-        "feature_tensors": {
-            "item_id": torch.LongTensor([[3, 3, 3, 2, 0, 1]]),
-            "sample_weight": torch.rand_like(positive_labels, dtype=torch.float32),
-        },
-        "padding_mask": torch.BoolTensor([[False, False, False, True, True, True]]),
-        "positive_labels": positive_labels,
-        "negative_labels": torch.LongTensor([2, 0]),
-        "target_padding_mask": torch.BoolTensor([[[False], [False], [True], [True], [True], [True]]]).repeat(
-            1, 1, num_multipositives
-=======
 def hidden_simple_batch_multipositive(hidden_simple_batch):
     hidden_simple_batch["positive_labels"] = hidden_simple_batch["positive_labels"].repeat(1, 1, 5)
     hidden_simple_batch["target_padding_mask"] = hidden_simple_batch["target_padding_mask"].repeat(1, 1, 5)
@@ -57,7 +40,6 @@ def hidden_simple_batch_multiclass_negatives():
         "negative_labels": torch.LongTensor([[2, 0], [1, 2]]),
         "target_padding_mask": torch.BoolTensor(
             [[[False], [False], [True], [True], [True], [True]], [[False], [False], [False], [True], [True], [True]]]
->>>>>>> Merge main
         ),
     }
 
