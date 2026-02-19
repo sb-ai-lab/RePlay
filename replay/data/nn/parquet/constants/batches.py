1222fb4fd380e135cd6ef14d8208253db7908618
VAGolubenko <vagolubenko@sberbank.ru>
2026-02-19 12:32:38 +0300

    Try another typing fix

--- replay/data/nn/parquet/constants/batches.py
+++ replay/data/nn/parquet/constants/batches.py
@@ -1,10 +1,8 @@
-from __future__ import annotations
-
 from collections.abc import Callable
 from typing import TypeAlias
 
 import torch
 
-GeneralValue: TypeAlias = "torch.Tensor" | "GeneralBatch"
+GeneralValue: TypeAlias = torch.Tensor | "GeneralBatch"
 GeneralBatch: TypeAlias = dict[str, GeneralValue]
 GeneralCollateFn: TypeAlias = Callable[[GeneralBatch], GeneralBatch]
