b4a5372f9615044828df78065ae96bf1cfa2fe08
Ekaterina Tsinovnik <ekaterinatsinovnik@gmail.com>
2026-02-20 08:34:27 +0000

    Add new Transforms

--- replay/nn/transform/__init__.py
+++ replay/nn/transform/__init__.py
@@ -1,19 +1,23 @@
 from .copy import CopyTransform
+from .equality_mask import EqualityMaskTransform
 from .grouping import GroupTransform
 from .negative_sampling import MultiClassNegativeSamplingTransform, UniformNegativeSamplingTransform
 from .next_token import NextTokenTransform
 from .rename import RenameTransform
 from .reshape import UnsqueezeTransform
+from .select import SelectTransform
 from .sequence_roll import SequenceRollTransform
 from .token_mask import TokenMaskTransform
 from .trim import TrimTransform
 
 __all__ = [
     "CopyTransform",
+    "EqualityMaskTransform",
     "GroupTransform",
     "MultiClassNegativeSamplingTransform",
     "NextTokenTransform",
     "RenameTransform",
+    "SelectTransform",
     "SequenceRollTransform",
     "TokenMaskTransform",
     "TrimTransform",
