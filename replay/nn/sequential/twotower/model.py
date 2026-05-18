8062b347eb3425b2f19ed639fa939e5db118235e
Ekaterina Tsinovnik <ekaterinatsinovnik@gmail.com>
2026-05-18 10:17:26 +0000

    Add custom hidden dim to TwoTower `from_params` method

--- replay/nn/sequential/twotower/model.py
+++ replay/nn/sequential/twotower/model.py
@@ -544,6 +544,7 @@ class TwoTower(torch.nn.Module):
         dropout: float = 0.3,
         excluded_features: list[str] | None = None,
         categorical_list_feature_aggregation_method: str = "sum",
+        hidden_dim: int | None = None,
     ) -> "TwoTower":
         """
         A class method for fast creation of the TwoTower instance.\n
@@ -572,6 +573,8 @@ class TwoTower(torch.nn.Module):
         :param categorical_list_feature_aggregation_method: Mode to aggregate tokens
             in token item representation (categorical list only).
             Default: ``"sum"``.
+        :param hidden_dim: hidden layer dimension of feed-forward network. If ``None``, uses ``embedding_dim``.
+            Defaults to ``None``.
         :return: an instance of TwoTower class.
         """
         from replay.nn.agg import SumAggregator
@@ -616,6 +619,7 @@ class TwoTower(torch.nn.Module):
                     num_blocks=num_blocks,
                     dropout=dropout,
                     activation="relu",
+                    hidden_dim=hidden_dim,
                 ),
                 query_tower_output_normalization=torch.nn.LayerNorm(embedding_dim),
                 item_encoder=SwiGLUEncoder(embedding_dim=embedding_dim, hidden_dim=2 * embedding_dim),
