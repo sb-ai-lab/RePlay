f12193d2335f6e99d4fd70d5e4d88c9d84ed24f3
Олег Сидоров <OSSidorov@sberbank.ru>
2026-08-04 12:05:40 +0300

    perf(nn): avoid embedding head copies

--- tests/nn/test_head.py
+++ tests/nn/test_head.py
@@ -21,3 +21,30 @@ def test_head_forward(shape_hidden, shape_embeddings, expected_shape):
     scores = head(hidden_states, item_embeddings)
 
     assert scores.shape == expected_shape
+
+
+@pytest.mark.parametrize(
+    "shape_hidden, shape_embeddings",
+    [
+        ((4, 3, 8), (100, 8)),
+        ((4, 8), (4, 100, 8)),
+    ],
+)
+def test_head_matches_transposed_matmul(shape_hidden, shape_embeddings):
+    hidden_states = torch.randn(shape_hidden, requires_grad=True)
+    item_embeddings = torch.randn(shape_embeddings, requires_grad=True)
+    reference_hidden_states = hidden_states.detach().clone().requires_grad_()
+    reference_item_embeddings = item_embeddings.detach().clone().requires_grad_()
+
+    actual = EmbeddingTyingHead()(hidden_states, item_embeddings)
+    transposed_embeddings = reference_item_embeddings.transpose(-1, -2).contiguous()
+    if reference_item_embeddings.dim() == 3:
+        reference = reference_hidden_states.unsqueeze(-2).matmul(transposed_embeddings).squeeze(-2)
+    else:
+        reference = reference_hidden_states.matmul(transposed_embeddings)
+    actual.sum().backward()
+    reference.sum().backward()
+
+    torch.testing.assert_close(actual, reference)
+    torch.testing.assert_close(hidden_states.grad, reference_hidden_states.grad)
+    torch.testing.assert_close(item_embeddings.grad, reference_item_embeddings.grad)
